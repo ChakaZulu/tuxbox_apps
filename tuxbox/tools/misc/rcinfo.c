69b4a1222ae88381a2907a24410367fd034ba330
Hunz <Hunz>
2001-04-26 16:51:05 +0000

    eek!

--- tuxbox/tools/misc/rcinfo.c
+++ tuxbox/tools/misc/rcinfo.c
@@ -16,7 +16,7 @@ char buf[2];
   exit(1);
  }
  if ((argc > 1) && (strcmp(argv[1],"-b") == 0))
-  ioctl(fd,RC_IOCTL_BCODES);
+  ioctl(fd,RC_IOCTL_BCODES,1);
  while(1) {
   read(fd,buf,2);
   printf("RC: %02X %02X\n",buf[0],buf[1]);
