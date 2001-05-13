87775822d4c1dbeb515a3089f84966e7bcc474e0
fnbrd <fnbrd>
2001-05-13 12:37:11 +0000

    Noch etwas verbessert.

--- tuxbox/neutrino/daemons/sectionsd/epg.cpp
+++ tuxbox/neutrino/daemons/sectionsd/epg.cpp
@@ -1,10 +1,30 @@
 //
-// Beispiel zur Benutzung der Klasse Section
+// $Id: epg.cpp,v 1.6 2001/05/13 12:37:11 fnbrd Exp $
 //
+// Beispiel zur Benutzung der SI class lib (dbox-II-project)
 //
-// $Id: epg.cpp,v 1.5 2001/05/13 00:39:30 fnbrd Exp $
+//    Homepage: http://dbox2.elxsi.de
+//
+//    Copyright (C) 2001 fnbrd (fnbrd@gmx.de)
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 // $Log: epg.cpp,v $
+// Revision 1.6  2001/05/13 12:37:11  fnbrd
+// Noch etwas verbessert.
+//
 // Revision 1.5  2001/05/13 00:39:30  fnbrd
 // Etwas aufgeraeumt.
 //
@@ -13,7 +33,6 @@
 //
 // Revision 1.2  2001/05/12 23:55:04  fnbrd
 // Ueberarbeitet, geht aber noch nicht ganz.
-//
 //
 
 //#define READ_PRESENT_INFOS
@@ -64,9 +83,9 @@ int main(int argc, char **argv)
 {
   time_t starttime, endtime;
 #ifdef READ_PRESENT_INFOS
-  EPGPresentSections epgset;
+  SIsectionsEIT epgset;
 #else
-  EPGSections epgset;
+  SIsectionsEITschedule epgset;
 #endif
 
   starttime=time(NULL);
