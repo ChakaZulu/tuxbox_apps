ce6045b9b49737f19a8777e6c014d315bc51c0d5
gillem <gillem>
2001-06-25 19:04:01 +0000

    - some changes

--- tuxbox/tools/misc/dvbnet.c
+++ tuxbox/tools/misc/dvbnet.c
@@ -1,14 +1,48 @@
-#include <sys/ioctl.h>
+/*
+ *   $Id: dvbnet.c,v 1.2 2001/06/25 19:04:01 gillem Exp $
+ *
+ *   dvbnet.c - setup dvb net device (dbox-II-project)
+ *
+ *   Homepage: http://dbox2.elxsi.de
+ *
+ *   Copyright (C) 2001 Gillem
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ *   $Log: dvbnet.c,v $
+ *   Revision 1.2  2001/06/25 19:04:01  gillem
+ *   - some changes
+ *
+ *
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <ost/net.h>
 
 main(int argc, char **argv)
 {
 	int fd;
-	int x,y;
 	struct dvb_net_if net_if;
+
+	memset( &net_if, 0, sizeof(struct dvb_net_if));
+
+	net_if.pid    = 0;
+	net_if.if_num = 0;
 
 	if((fd = open("/dev/ost/net0",O_RDWR|O_NONBLOCK)) < 0)
 	{
@@ -19,15 +53,18 @@ main(int argc, char **argv)
 	if ( (ioctl(fd,NET_ADD_IF,&net_if) < 0))
 	{
 		perror("NET ADD: ");
+		close(fd);
 		return -1;
 	}
 
-	if ( (ioctl(fd,NET_REMOVE_IF,&net_if) < 0))
+	if ( (ioctl(fd,NET_REMOVE_IF,net_if.if_num) < 0))
 	{
 		perror("NET REMOVE: ");
+		close(fd);
 		return -1;
 	}
 
 	close(fd);
+
 	return 0;
 }
