2ab39f7c66aa00ceabd0934a1af3c69a1c4965d0
Ghostrider <Ghostrider>
2001-10-07 21:02:47 +0000

    setting_helpers.cpp und setting_helpers.h wieder geadded... sind wohl irgendwie abhanden gekommen. Nun kompiliert NeutrinoNG wieder. Ghostrider

---
+++ tuxbox/neutrino/src/system/setting_helpers.h
@@ -0,0 +1,46 @@
+#ifndef __setting_helpers__
+#define __setting_helpers__
+
+#include "../widget/menue.h"
+
+class CColorSetupNotifier : public CChangeObserver
+{
+	public:
+		bool changeNotify(string OptionName);
+};
+
+class CAudioSetupNotifier : public CChangeObserver
+{
+    public:
+    	bool changeNotify(string OptionName);
+};
+
+class CVideoSetupNotifier : public CChangeObserver
+{
+    public:
+    	bool changeNotify(string OptionName);
+};
+
+class CLanguageSetupNotifier : public CChangeObserver
+{
+	public:
+		bool changeNotify(string OptionName);
+};
+
+class CKeySetupNotifier : public CChangeObserver
+{
+	public:
+		bool changeNotify(string OptionName);
+};
+
+class CAPIDChangeExec : public CMenuTarget
+{
+	public:
+        int exec(CMenuTarget* parent, string actionKey);
+};
+
+void setDefaultGateway(char* ip);
+void setNetworkAddress(char* ip, char* netmask, char* broadcast);
+void setNameServer(char* ip);
+
+#endif
