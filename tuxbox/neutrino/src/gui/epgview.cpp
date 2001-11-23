c8e3bf73cdafec1173e9f6a0c4d94f81be7ed70b
McClean <McClean>
2001-11-23 17:25:45 +0000

    dont hide epg automaticly

--- tuxbox/neutrino/src/gui/epgview.cpp
+++ tuxbox/neutrino/src/gui/epgview.cpp
@@ -30,9 +30,12 @@
 */
 
 //
-// $Id: epgview.cpp,v 1.23 2001/11/15 11:42:41 McClean Exp $
+// $Id: epgview.cpp,v 1.24 2001/11/23 17:25:45 McClean Exp $
 //
 // $Log: epgview.cpp,v $
+// Revision 1.24  2001/11/23 17:25:45  McClean
+// dont hide epg automaticly
+//
 // Revision 1.23  2001/11/15 11:42:41  McClean
 // gpl-headers added
 //
@@ -328,7 +331,7 @@ void CEpgData::show( string channelName, unsigned int onid_tsid, unsigned long l
     	int scrollCount;
     	while(loop)
     	{
-    		int key = g_RCInput->getKey(150);
+    		int key = g_RCInput->getKey();
 
     		scrollCount = medlinecount;
 
