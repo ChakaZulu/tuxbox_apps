be92eb88df90a2dfd44bc3213b7b679f5f618b9f
rasc <rasc>
2001-12-01 12:46:48 +0000

    pespacket weitergestrickt, leider z.Zt. zuwenig Zeit um es richtig fertig zu machen.

--- dvb/dvbsnoop/src/pes/pespacket.c
+++ dvb/dvbsnoop/src/pes/pespacket.c
@@ -1,5 +1,5 @@
 /*
-$Id: pespacket.c,v 1.2 2001/10/02 21:52:44 rasc Exp $
+$Id: pespacket.c,v 1.3 2001/12/01 12:46:48 rasc Exp $
 
    -- PES Decode/Table section
 
@@ -7,10 +7,12 @@ $Id: pespacket.c,v 1.2 2001/10/02 21:52:44 rasc Exp $
 
 
 $Log: pespacket.c,v $
+Revision 1.3  2001/12/01 12:46:48  rasc
+pespacket weitergestrickt, leider z.Zt. zuwenig Zeit um es richtig fertig zu machen.
+
 Revision 1.2  2001/10/02 21:52:44  rasc
 - init der time_delta
 - PES erweitert, PES arbeitet im read() noch nicht richtig!!
-- muss tmbinc fragem, ob ich Mist baue, oder der Treiber (??)
 
 Revision 1.1  2001/09/30 13:05:20  rasc
 dvbsnoop v0.7  -- Commit to CVS
@@ -27,6 +29,7 @@ dvbsnoop v0.7  -- Commit to CVS
 
 
 int PES_decode2 (u_char *b, int len, int pid);
+void print_xTS_field (u_char *b, int bit_offset);
 
 
 
@@ -57,10 +60,13 @@ void decodePES_buf (u_char *b, int len, int pid)
  p.PES_packet_length			 = getBits (b, 0, 32, 16);
 
 
- out_nl (3," Packet_Start_CODE has to be 0x000001 !!!!  $$$$ ToDO \n");
+ if (p.packet_start_code_prefix != 0x000001) {
+    out_nl (3," Packet_Start_CODE is wrong (= no PES)!  Following (wrong!?) decoded:\n");
+ }
 
  out_nl     (3,"Packet_start_code_prefix: %06lx",p.packet_start_code_prefix);
- out_S2B_NL (3,"Stream_id: ",p.stream_id,dvbstrPESstream_ID(p.stream_id));
+ out_SB_NL (3,"Stream_id: ",p.stream_id);
+//$$$ out_S2B_NL (3,"Stream_id: ",p.stream_id,dvbstrPESstream_ID(p.stream_id));
  out_SW_NL  (3,"PES_packet_length: ",p.PES_packet_length);
 
  b   += 6;
@@ -105,8 +111,146 @@ void decodePES_buf (u_char *b, int len, int pid)
 int  PES_decode2 (u_char *b, int len, int pid)
 
 {
+ /* IS13818-1  2.4.3.6  */
+
+ typedef struct  _PES2_Packet {
+        u_int     reserved1;
+        u_int     PES_scrambling_control;
+        u_int     PES_priority;
+        u_int     data_alignment_indicator;
+        u_int     copyright;
+        u_int     original_or_copy;
+        u_int     PTS_DTS_flags;
+        u_int     ESCR_flag;
+        u_int     ES_rate;
+        u_int     DSM_trick_mode_flag;
+        u_int     additional_copy_info_flag;
+        u_int     PES_CRC_flag;
+        u_int     PES_extension_flag;
+        u_int     PES_header_data_length;
+
+	// N ... data
+
+ } PES2_Packet;
 
 
+
+
+ PES2_Packet  p;
+ int          len2;
+ int          n;
+
+
+ p.reserved1				= getBits (b, 0,  0, 2);
+ p.PES_scrambling_control		= getBits (b, 0,  2, 2);
+ p.PES_priority				= getBits (b, 0,  4, 1);
+ p.data_alignment_indicator		= getBits (b, 0,  5, 1);
+ p.copyright				= getBits (b, 0,  6, 1);
+ p.original_or_copy			= getBits (b, 0,  7, 1);
+ p.PTS_DTS_flags			= getBits (b, 0,  8, 2);
+ p.ESCR_flag				= getBits (b, 0, 10, 1);
+ p.ES_rate				= getBits (b, 0, 11, 1);
+ p.DSM_trick_mode_flag			= getBits (b, 0, 12, 1);
+ p.additional_copy_info_flag		= getBits (b, 0, 13, 1);
+ p.PES_CRC_flag				= getBits (b, 0, 14, 1);
+ p.PES_extension_flag			= getBits (b, 0, 15, 1);
+ p.PES_header_data_length		= getBits (b, 0, 16, 8);
+
+
+
+ out_SB_NL  (6,"reserved1: ",p.reserved1);
+// Routine fehlt noch
+// out_S2B_NL (3,"PES_scrambling_control: ",p.PES_scrambling_control,
+//	dvbstrPESscramling_control_TYPE(p.PES_scrambling_control));
+ out_SB_NL  (3,"PES_priority: ",p.PES_priority);
+ out_SB_NL  (3,"data_alignment_indicator: ",p.data_alignment_indicator);
+ out_SB_NL  (3,"copyright: ",p.copyright);
+ out_SB_NL  (3,"original_or_copy: ",p.original_or_copy);
+ out_SB_NL  (3,"PTS_DTS_flags: ",p.PTS_DTS_flags);
+ out_SB_NL  (3,"ES_rate: ",p.ES_rate);
+ out_SB_NL  (3,"additional_copy_info_flag: ",p.additional_copy_info_flag);
+ out_SB_NL  (3,"PES_CRC_flag: ",p.PES_CRC_flag);
+ out_SB_NL  (3,"PES_extension_flag: ",p.PES_extension_flag);
+ out_SB_NL  (3,"PES_header_data_length: ",p.PES_header_data_length);
+
+ b += 3;
+
+
+ if (p.PTS_DTS_flags & 0x02) {    // 10
+   out_nl (3,"PTS: ");
+   indent (+1);
+   out_SB_NL  (3,"Fixed: ", getBits (b, 0,  0, 4) );
+   print_xTS_field (b, 4) ;
+   indent (-1);
+   b += 5;
+ }
+
+ if (p.PTS_DTS_flags == 0x03) {    // 10 + 01
+   // PTS from "if" before...
+   out_nl (3,"DTS: ");
+   indent (+1);
+   out_SB_NL  (3,"Fixed: ", getBits (b, 0,  0, 4) );
+   print_xTS_field (b, 4) ;
+   indent (-1);
+   b += 5;
+ }
+
+ if (p.ESCR_flag == 0x01) {
+   out_nl (3,"ESCR_base: ");
+   indent (+1);
+   out_SB_NL  (3,"Reserved: ", getBits (b, 0,  0, 2) );
+   print_xTS_field (b, 2) ;
+
+   out_SW_NL  (3,"ESCR_extension: ", getBits (b, 0, 38, 9) );
+   out_SW_NL  (3,"marker_bit: ",     getBits (b, 0, 47, 1) );
+   indent (-1);
+   b += 6;
+
+ }
+
+
+
+
+ out_nl (3,".... rest still unimplemented.... ");
+ // $$$ hier muss auch noch einiges gemacht werden (aber zur Zeit
+ // $$$ so gut wie keine Zeit
 
 
 }
+
+
+
+
+void print_xTS_field (u_char *b, int bit_offset) 
+
+{
+ typedef struct  _xTS_Field {
+        u_int     xTS_32_30;
+        u_int     marker_bit_1;
+        u_int     xTS_29_15;
+        u_int     marker_bit_2;
+        u_int     xTS_14_0;
+        u_int     marker_bit_3;
+ } xTS_Field;
+
+ xTS_Field  f;
+ int        bo = bit_offset;
+
+
+ f.xTS_32_30				= getBits (b, 0, bo+ 0, 3);
+ f.marker_bit_1				= getBits (b, 0, bo+ 3, 1);
+ f.xTS_29_15				= getBits (b, 0, bo+ 4,15);
+ f.marker_bit_2				= getBits (b, 0, bo+19, 1);
+ f.xTS_14_0				= getBits (b, 0, bo+20,15);
+ f.marker_bit_3				= getBits (b, 0, bo+35, 1);
+
+ out_SB_NL  (4,"Bit 32-30: ",f.xTS_32_30);
+ out_SB_NL  (4,"Marker_bit_1: ",f.marker_bit_1);
+ out_SB_NL  (4,"Bit 29-15: ",f.xTS_29_15);
+ out_SW_NL  (4,"Marker_bit_2: ",f.marker_bit_2);
+ out_SB_NL  (4,"Bit 14-0: ",f.xTS_14_0);
+ out_SW_NL  (4,"Marker_bit_3: ",f.marker_bit_3);
+
+}
+
+
