7f7933c9c73113be447c4c8502d3de62ee37d36c
McClean <McClean>
2001-12-14 11:14:32 +0000

    fix includes

--- tuxbox/neutrino/lib/controldclient/controldclient.cpp
+++ tuxbox/neutrino/lib/controldclient/controldclient.cpp
@@ -29,8 +29,7 @@
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include "controld.h"
-#include "../global.h"
+#include "controldclient.h"
 
 
 CControldClient::CControldClient()
