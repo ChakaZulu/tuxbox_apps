a3540ad39d3ee93164b6639fd1240f87d3dedc8c
fx2 <fx2>
2002-01-09 18:29:47 +0000

    inserted tags for save highscore

--- tuxbox/plugins/include/plugin.h
+++ tuxbox/plugins/include/plugin.h
@@ -20,7 +20,10 @@ typedef int	(*PluginExec)( PluginParam *par );
 #define	P_ID_FBUFFER	"fd_framebuffer"
 #define	P_ID_RCINPUT	"fd_rcinput"
 #define	P_ID_LCD		"fd_lcd"
-#define	P_ID_NOPIG		"no_pig"			// 1: plugin dont show internal pig
-#define P_ID_VTXTPID		"pid_vtxt"
+#define	P_ID_NOPIG		"no_pig"		// 1: plugin dont show internal pig
+#define P_ID_VTXTPID	"pid_vtxt"
+#define P_ID_PROXY		"proxy"			// set proxy for save into highscore
+#define P_ID_PROXY_USER	"proxy_user"	// format "user:pass"
+#define P_ID_HSCORE		"hscore"		// highscore-server (as url)
 
 #endif
