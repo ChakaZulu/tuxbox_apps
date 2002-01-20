34c6af9f64f2f2d71ac90a96ec5799ed3af31fe6
obi <obi>
2002-01-20 22:44:29 +0000

    removed unneeded files

--- tuxbox/enigma/src/ezap_dyn.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ezap_dyn_h_
-#define __ezap_dyn_h_
-
-class eHTTPDynPathResolver;
-
-void ezapInitializeDyn(eHTTPDynPathResolver *dyn_resolver);
-
-#endif
--- tuxbox/enigma/src/ezap_xmlrpc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ezap_xmlrpc_h
-#define __ezap_xmlrpc_h
-
-void ezapInitializeXMLRPC(eHTTPDynPathResolver *dyn_resolver);
-
-#endif
