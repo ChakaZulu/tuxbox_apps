f6e8207bdb31b8b5bdef36c3b926d10ed115f90f
McClean <McClean>
2002-01-21 00:30:44 +0000

    better epg-display (fixme: statusbar needed for date/time and buttondefs)

--- tuxbox/enigma/src/enigma_event.cpp
+++ tuxbox/enigma/src/enigma_event.cpp
@@ -72,47 +72,62 @@ eEventDisplay::~eEventDisplay()
 
 void eEventDisplay::setEvent(EITEvent *event)
 {
+
 	if (event)
 	{
-		QString s;
+		QString title=0, long_description="";
+		QString eventDate="";
+		QString eventTime="";
+
 		tm *begin=event->start_time!=-1?localtime(&event->start_time):0;
-		if (begin)
-			s.sprintf("%02d:%02d", begin->tm_hour, begin->tm_min);
 		time_t endtime=event->start_time+event->duration;
 		tm *end=event->start_time!=-1?localtime(&endtime):0;
-		if (end)
-			s+=QString().sprintf(" - %02d:%02d", end->tm_hour, end->tm_min);
 
 		if (begin)
-			s+=QString().sprintf(", %d.%d.%4d", begin->tm_mday, begin->tm_mon+1, begin->tm_year+1900);
+			eventTime.sprintf("%02d:%02d", begin->tm_hour, begin->tm_min);
+		if (end)
+			eventTime+=QString().sprintf(" - %02d:%02d", end->tm_hour, end->tm_min);
+
+		if (begin)
+			eventDate=QString().sprintf("%d.%d.%4d", begin->tm_mday, begin->tm_mon+1, begin->tm_year+1900);
 	
-		QString title=0, long_description="";
+
 		for (QListIterator<Descriptor> d(event->descriptor); d.current(); ++d)
 		{
 			if (d.current()->Tag()==DESCR_SHORT_EVENT)
 			{
 				ShortEventDescriptor *s=(ShortEventDescriptor*)d.current();
 				title=s->event_name;
-				long_description+=s->text;
-				if (s->text.length() > 0) long_description+="\n\n";
+				if ((s->text.length() > 0) && (s->text!=title))
+				{
+					long_description+=s->text;
+					long_description+="\n\n";
+				}
 			} else if (d.current()->Tag()==DESCR_EXTENDED_EVENT)
 			{
 				ExtendedEventDescriptor *ss=(ExtendedEventDescriptor*)d.current();
 				long_description+=ss->item_description;
 			}
 		}
-		QString str=service;
-		if (title)
-			str+=" - "+title;
 
-    if (s.length())
-			str+=" // "+s+" \\\\ ";
+		QString str="";
+		if (!title)
+			title ="keine Information verfügbar";
+
+
+		str = long_description;
+	    if (eventDate.length())
+			str+="\n\nSendezeit: " + eventDate+ ", " + eventTime;
 
 		qDebug("setting string: %s", (const char*)long_description);
-		setText(str);
-		text->setText(long_description);
-	} else
-		setText(service+" - no description available");
+		setText(title);
+		text->setText(str);
+	} 
+	else
+	{
+		setText(service);
+		text->setText("keine Beschreibung verfügbar");
+	}
 }
 
 void eEventDisplay::setList(const QList<EITEvent> &e)
