04e978566ba41731264165ad0c25b55cb79b2107
tmbinc <tmbinc>
2002-02-07 21:19:56 +0000

    added eAutoInit

---
+++ tuxbox/enigma/include/enigma/system/init.h
@@ -0,0 +1,87 @@
+#ifndef __init_h
+#define __init_h
+
+#include <list>
+#include <utility>
+
+class eAutoInit;
+
+class eInit
+{
+	static std::list<std::pair<int,eAutoInit*> > *cl;
+	friend class eAutoInit;
+	static void add(int trl, eAutoInit *c);
+	static void remove(int trl, eAutoInit *c);
+	static int rl;
+public:
+	eInit();
+	~eInit();
+	static void setRunlevel(int rlev);
+};
+
+class eAutoInit
+{
+	friend class eInit;
+	virtual void initNow()=0;
+	virtual void closeNow()=0;
+protected:
+	int rl;
+	char *description;
+public:
+	eAutoInit(int rl, char *description): rl(rl), description(description)
+	{
+		eInit::add(rl, this);
+	}
+	~eAutoInit()
+	{
+		eInit::remove(rl, this);
+	}
+	const char *getDescription() const { return description; };
+};
+
+template<class T1, int runl, class T2> class
+eAutoInitP1: protected eAutoInit
+{
+	T1 *t;
+	const T2 &arg;
+	void initNow()
+	{
+		t=new T1(arg);
+	}
+	void closeNow()
+	{
+		delete t;
+	}
+public:
+	operator T1*()
+	{
+		return t;
+	}
+	eAutoInitP1(const T2 &arg, char *description): eAutoInit(runl, description), arg(arg)
+	{
+	}
+};
+
+template<class T1, int runl> class
+eAutoInitP0: protected eAutoInit
+{
+	T1 *t;
+	void initNow()
+	{
+		t=new T1();
+	}
+	void closeNow()
+	{
+		delete t;
+	}
+public:
+	operator T1*()
+	{
+		return t;
+	}
+	eAutoInitP0(char *description): eAutoInit(runl, description)
+	{
+	}
+};
+
+#endif
---
+++ tuxbox/enigma/lib/system/init.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "init.h"
+
+int eInit::rl=-1;
+std::list<std::pair<int,eAutoInit*> > *eInit::cl;
+
+void eInit::add(int trl, eAutoInit *c)
+{
+	if (!cl)
+	{
+		cl=new std::list<std::pair<int,eAutoInit*> >;
+		printf("creating list..\n");
+	}
+	printf("add %d (%s)\n", trl, c->getDescription());
+	cl->push_back(std::pair<int,eAutoInit*>(trl, c));
+	if (rl>=trl)
+		c->initNow();
+}
+
+void eInit::remove(int trl, eAutoInit *c)
+{
+	cl->remove(std::pair<int,eAutoInit*>(trl, c));
+	if (rl>=trl)
+		c->closeNow();
+}
+
+eInit::eInit()
+{
+}
+
+eInit::~eInit()
+{
+	setRunlevel(-1);
+	delete cl;
+}
+
+void eInit::setRunlevel(int nrl)
+{
+	while (nrl>rl)
+	{
+		rl++;
+		for (std::list<std::pair<int,eAutoInit*> >::iterator i(cl->begin()); i!=cl->end(); ++i)
+		{
+			if ((*i).first == rl)
+			{
+				printf("+ (%d) %s\n", rl, (*i).second->getDescription());
+				(*i).second->initNow();
+			}
+		}
+	}
+	
+	while (nrl<rl)
+	{
+		for (std::list<std::pair<int,eAutoInit*> >::iterator i(cl->begin()); i!=cl->end(); ++i)
+			if ((*i).first == rl)
+			{
+				printf("- (%d) %s\n", rl, (*i).second->getDescription());
+				(*i).second->closeNow();
+			}
+		rl--;
+	}
+	printf("reached rl %d\n", rl);
+}
