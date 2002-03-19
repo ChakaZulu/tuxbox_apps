f2a64cd7cbc3d2e1b191089bc5a025083d1e8bc9
McClean <McClean>
2002-03-19 01:21:11 +0000

    initial

---
+++ tuxbox/neutrino/daemons/controld/driver/audio.h
@@ -0,0 +1,50 @@
+/*
+	Control-Daemon  -   DBoxII-Project
+
+	Copyright (C) 2001 Steffen Hehn 'McClean'
+	Homepage: http://dbox.cyberphoria.org/
+
+
+
+	License: GPL
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __audiocontrol__
+#define __audiocontrol__
+
+
+class audioControl
+{
+
+	enum 
+	{	
+		AUDIO_STEREO = 1,
+		AUDIO_MONO_LEFT,
+		AUDIO_MONO_RIGHT
+	};
+
+	
+
+};
+
+
+
+
+
+
+
+#endif
