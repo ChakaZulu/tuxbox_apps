a8541e14a32965b059d37a92dff057652a633ab5
tmbinc <tmbinc>
2002-04-19 22:54:26 +0000

    position geaendert (bis das mal skinnbar ist)

--- tuxbox/enigma/src/enigma_mainmenu.cpp
+++ tuxbox/enigma/src/enigma_mainmenu.cpp
@@ -10,12 +10,11 @@
 #include "enigma_lcd.h"
 #include "elabel.h"
 #include "epgcache.h"
-#include "epixmap.h"
 
 eMainMenu::eMainMenu()
 {
 	eZapLCD *pLCD=eZapLCD::getInstance();
-	window=new eLBWindow("enigma 0.1" , eListbox::tBorder, 12, eSkin::getActive()->queryValue("fontsize", 20), 240);
+	window=new eLBWindow("enigma" , eListbox::tBorder, 12, eSkin::getActive()->queryValue("fontsize", 20), 400);
 	window->setLCD(pLCD->lcdMenu->Title, pLCD->lcdMenu->Element);
 	window->move(ePoint(70, 150));
 	CONNECT((new eListboxEntryText(window->list, "TV Mode"))->selected, eMainMenu::sel_close);
