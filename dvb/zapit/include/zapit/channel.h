5f852a9d0296e87576c85b221b059fb1b3b43f6a
obi <obi>
2002-05-13 17:18:05 +0000

    channel class

---
+++ dvb/zapit/include/zapit/channel.h
@@ -0,0 +1,122 @@
+/*
+ * $Id: channel.h,v 1.1 2002/05/13 17:18:05 obi Exp $
+ *
+ * (C) 2002 by Andreas Oberritter <obi@tuxbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __channel_h__
+#define __channel_h__
+
+/* system */
+#include <string>
+
+/* zapit */
+#include <zapci/ci.h>
+
+#define MAX_AUDIO_PIDS	13
+
+class CZapitAudioChannel
+{
+	public:
+		unsigned short pid;
+		bool isAc3;
+		std::string description;
+		unsigned char componentTag;
+};
+
+class CZapitChannel
+{
+	private:
+		/* channel name */
+		std::string name;
+
+		/* pids of this channel */
+		std::vector <CZapitAudioChannel *> audioChannels;
+		unsigned short pcrPid;
+		unsigned short pmtPid;
+		unsigned short teletextPid;
+		unsigned short videoPid;
+
+		/* set true when pids are set up */
+		bool pidsFlag;
+
+		/* last selected audio channel */
+		unsigned char currentAudioChannel;
+
+		/* number of audio channels */
+		unsigned char audioChannelCount;
+
+		/* number in channel list */
+		unsigned short channelNumber;
+
+		/* read only properties, set by constructor */
+		unsigned short serviceId;
+		unsigned short transportStreamId;
+		unsigned short originalNetworkId;
+		unsigned char serviceType;
+		unsigned char DiSEqC;
+
+		/* the conditional access program map table of this channel */
+		CCaPmt * caPmt;
+
+	public:
+		/* constructor, desctructor */
+		CZapitChannel (std::string p_name, unsigned short p_sid, unsigned short p_tsid, unsigned short p_onid, unsigned char p_service_type, unsigned short p_chan_nr, unsigned char p_DiSEqC);
+		~CZapitChannel ();
+
+		/* get methods - read only variables */
+		unsigned short getServiceId()		{ return serviceId; }
+		unsigned short getTransportStreamId()	{ return transportStreamId; }
+		unsigned short getOriginalNetworkId()	{ return originalNetworkId; }
+		unsigned char getServiceType()		{ return serviceType; }
+		unsigned char getDiSEqC()		{ return DiSEqC; }
+		unsigned int getOnidSid()		{ return (originalNetworkId << 16) | serviceId; }
+		unsigned int getTsidOnid()		{ return (transportStreamId << 16) | originalNetworkId; }
+
+		/* get methods - read and write variables */
+		std::string getName()			{ return name; }
+		unsigned char getAudioChannelCount()	{ return audioChannelCount; }
+		unsigned short getPcrPid()		{ return pcrPid; }
+		unsigned short getPmtPid()		{ return pmtPid; }
+		unsigned short getTeletextPid()		{ return teletextPid; }
+		unsigned short getVideoPid()		{ return videoPid; }
+		unsigned short getChannelNumber()	{ return channelNumber; }
+		bool getPidsFlag()			{ return pidsFlag; }
+		CCaPmt * getCaPmt()			{ return caPmt; }
+
+		CZapitAudioChannel * getAudioChannel (unsigned char index = 0xFF);
+		unsigned short getAudioPid (unsigned char index = 0xFF);
+
+		int addAudioChannel(unsigned short pid, bool isAc3, string description, unsigned char componentTag);
+
+		/* set methods */
+		void setName(std::string pName)				{ name = pName; }
+		void setAudioChannel(unsigned char pAudioChannel)	{ currentAudioChannel = pAudioChannel; }
+		void setPcrPid(unsigned short pPcrPid)			{ pcrPid = pPcrPid; }
+		void setPmtPid(unsigned short pPmtPid)			{ pmtPid = pPmtPid; }
+		void setTeletextPid(unsigned short pTeletextPid)	{ teletextPid = pTeletextPid; }
+		void setVideoPid(unsigned short pVideoPid)		{ videoPid = pVideoPid; }
+		void setChannelNumber(unsigned short pChannelNumber)	{ channelNumber = pChannelNumber; }
+		void setPidsFlag()					{ pidsFlag = true; }
+		void setCaPmt(CCaPmt * pCaPmt)				{ caPmt = pCaPmt; }
+
+		/* cleanup methods */
+		void resetPids();
+};
+
+#endif /* __channel_h__ */
---
+++ dvb/zapit/src/channel.cpp
@@ -0,0 +1,137 @@
+/*
+ * $Id: channel.cpp,v 1.1 2002/05/13 17:18:05 obi Exp $
+ *
+ * (C) 2002 by Andreas Oberritter <obi@tuxbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "channel.h"
+
+CZapitChannel::CZapitChannel (std::string p_name, unsigned short p_sid, unsigned short p_tsid, unsigned short p_onid, unsigned char p_service_type, unsigned short p_chan_nr, unsigned char p_DiSEqC)
+{
+	unsigned char i;
+
+	name = p_name;
+	serviceId = p_sid;
+	transportStreamId = p_tsid;
+	originalNetworkId = p_onid;
+	serviceType = p_service_type;
+	channelNumber = p_chan_nr;
+	DiSEqC = p_DiSEqC;
+
+	caPmt = NULL;
+
+	for (i = 0; i < MAX_AUDIO_PIDS; i++)
+	{
+		audioChannels[i] = NULL;
+	}
+
+	currentAudioChannel = 0;
+	audioChannelCount = 0;
+
+	pcrPid = 0;
+	pmtPid = 0;
+	teletextPid = 0;
+	videoPid = 0;
+
+	pidsFlag = false;
+}
+
+CZapitChannel::~CZapitChannel ()
+{
+	unsigned char i;
+
+	for (i = 0; i < audioChannelCount; i++)
+	{
+		delete audioChannels[i];
+	}
+
+	delete caPmt;
+}
+
+CZapitAudioChannel * CZapitChannel::getAudioChannel (unsigned char index)
+{
+	if (index == 0xFF)
+	{
+		return audioChannels[currentAudioChannel];
+	}
+
+	if (index < audioChannelCount)
+	{
+		return audioChannels[index];
+	}
+
+	return NULL;
+}
+
+unsigned short CZapitChannel::getAudioPid (unsigned char index = 0xFF)
+{
+	if (index == 0xFF)
+	{
+		return audioChannels[currentAudioChannel]->pid;
+	}
+	if (index < audioChannelCount)
+	{
+		return audioChannels[index]->pid;
+	}
+
+	return 0;
+}
+
+int CZapitChannel::addAudioChannel(unsigned short pid, bool isAc3, string description, unsigned char componentTag)
+{
+	unsigned char i;
+
+	for (i = 0; i < audioChannelCount; i++)
+	{
+		if (audioChannels[i]->pid == pid)
+		{
+			return -1;
+		}
+	}
+
+	audioChannels[audioChannelCount] = new CZapitAudioChannel();
+	audioChannels[audioChannelCount]->pid = pid;
+	audioChannels[audioChannelCount]->isAc3 = isAc3;
+	audioChannels[audioChannelCount]->description = description;
+	audioChannels[audioChannelCount]->componentTag = componentTag;
+	audioChannelCount++;
+
+	return 0;
+}
+
+void CZapitChannel::resetPids()
+{
+	unsigned char i;
+
+	for (i = 0; i < audioChannelCount; i++)
+	{
+		delete audioChannels[i];
+		audioChannels[i] = NULL;
+	}
+
+	currentAudioChannel = 0;
+	audioChannelCount = 0;
+
+	pcrPid = 0;
+	pmtPid = 0;
+	teletextPid = 0;
+	videoPid = 0;
+
+	pidsFlag = false;
+}
+
