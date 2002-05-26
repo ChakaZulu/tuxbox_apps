38b40878d6f7ad48fb60136220e69ac922451032
Ghostrider <Ghostrider>
2002-05-26 16:45:53 +0000

    epgwindow now use new Listbox

--- tuxbox/enigma/src/epgwindow.cpp
+++ tuxbox/enigma/src/epgwindow.cpp
@@ -2,40 +2,44 @@
 #include "enigma_event.h"
 
 #include <algorithm>
+#include <iomanip>
 
 #include <core/driver/rc.h>
 #include <core/gui/eskin.h>
 #include <core/dvb/edvb.h>
 
 
-eString eListboxEntryEPG::getText(int col) const
-{
-		tm* t = localtime(&event->start_time);
-		eString DateTime;
-		DateTime.sprintf("%2d.%d, %02d:%02d   ", t->tm_mday, t->tm_mon+1, t->tm_hour, t->tm_min);
-		for (ePtrList<Descriptor>::iterator d(event->descriptor); d != event->descriptor.end(); ++d)
+eListBoxEntryEPG::eListBoxEntryEPG(const eit_event_struct* evt, eListBox<eListBoxEntryEPG> *listbox)
+		:eListBoxEntryTextStream((eListBox<eListBoxEntryTextStream>*)listbox), event(evt)	
+{	
+		for (ePtrList<Descriptor>::iterator d(event.descriptor); d != event.descriptor.end(); ++d)
 		{
 			Descriptor *descriptor=*d;
 			if (descriptor->Tag()==DESCR_SHORT_EVENT)
 			{
-				ShortEventDescriptor *ss=(ShortEventDescriptor*)descriptor;
-				return DateTime+ss->event_name;
+				tm* t = localtime(&event.start_time);
+ 		
+				text 	<< std::setw(2) << t->tm_mday << '.' << t->tm_mon+1 << ", "
+							<< std::setw(2) << std::setfill('0') << t->tm_hour << ':' << std::setw(2) << t->tm_min << "  "
+ 							<< ((ShortEventDescriptor*)descriptor)->event_name;
+
+				return;
 			}
 		}
-		return eString("no_name");
+		text << "no_name";
 }
 
 void eEPGWindow::fillEPGList()
 {
-	setText(eString("EPG - ")+current->service_name.c_str());
+	setText(eString("EPG - ")+current->service_name);
 	eDebug("get EventMap for onid: %02x, sid: %02x\n", current->original_network_id, current->service_id);
 	const eventMap* evt = eEPGCache::getInstance()->getEventMap(current->original_network_id, current->service_id);
 	eventMap::const_iterator It;
 	for (It = evt->begin(); It != evt->end(); It++)
-		new eListboxEntryEPG(new EITEvent(*It->second) , &list);
+		new eListBoxEntryEPG(*It->second, &list);
 }
 
-void eEPGWindow::entrySelected(eListboxEntry *entry)
+void eEPGWindow::entrySelected(eListBoxEntryEPG *entry)
 {
 	if (!entry)
 		close(0);
@@ -43,27 +47,27 @@ void eEPGWindow::entrySelected(eListboxEntry *entry)
 	{	
 		int ret;
 		hide();
-		eEventDisplay ei(eDVB::getInstance()->service->service_name.c_str(), 0, ((eListboxEntryEPG*)entry)->event);
+		eEventDisplay ei(eDVB::getInstance()->service->service_name.c_str(), 0, &entry->event);
 		ei.show();
 		while(ret = ei.exec())
 		{
-			eListboxEntryEPG* tmp;
+			eListBoxEntryEPG* tmp;
 
 			if (ret==-1)
-				tmp=(eListboxEntryEPG*)list.goPrev();
+				tmp=list.goPrev();
 			else if (ret == 1)
-				tmp=(eListboxEntryEPG*)list.goNext();
+				tmp=list.goNext();
 
 			if (tmp)
-				ei.setEvent(tmp->event);					
+				ei.setEvent(&tmp->event);					
 		}
 		ei.hide();
 		show();
 	}
 }
 
-eEPGWindow::eEPGWindow(eService* service):current(service),
-								eLBWindow("Select Service...", 16, eSkin::getActive()->queryValue("fontsize", 20), 600)
+eEPGWindow::eEPGWindow(eService* service)
+:current(service),eListBoxWindow<eListBoxEntryEPG>("Select Service...", 16, eSkin::getActive()->queryValue("fontsize", 20), 600)
 {
 	move(ePoint(50, 50));
 	list.setActiveColor(eSkin::getActive()->queryScheme("eServiceSelector.highlight"));
--- tuxbox/enigma/src/epgwindow.h
+++ tuxbox/enigma/src/epgwindow.h
@@ -1,39 +1,24 @@
 #ifndef __epgwindow_h
 #define __epgwindow_h
 
-#include "elbwindow.h"
+#include "eListBox.h"
 #include "epgcache.h"
 #include <libsig_comp.h>
 
-class eListboxEntryEPG: public eListboxEntry
+class eListBoxEntryEPG: public eListBoxEntryTextStream
 {
+	friend class eListBox<eListBoxEntryEPG>;
 public:
-	EITEvent* event;
-	eString getText(int col=0) const;
-	inline eListboxEntryEPG(EITEvent* evt, eListbox *listbox): eListboxEntry(listbox), event(evt)	{	}
-	inline ~eListboxEntryEPG(){ delete event; }
-	inline int operator<(const eListboxEntry& q) const;
-	inline int operator==(const eListboxEntry& q) const;
+	EITEvent event;
+	eListBoxEntryEPG(const eit_event_struct* evt, eListBox<eListBoxEntryEPG> *listbox);
 };
 
-int eListboxEntryEPG::operator<(const eListboxEntry & q) const
-{
-	// much faster as string compare
-	return 0;//( event->start_time < ((eListboxEntryEPG*) &q)->event->start_time );
-}
-
-int eListboxEntryEPG::operator==(const eListboxEntry & q) const
-{
-	return (event->start_time == ((eListboxEntryEPG*) &q)->event->start_time );
-}
-
-class eEPGWindow: public eLBWindow
+class eEPGWindow: public eListBoxWindow<eListBoxEntryEPG>
 {
 	eService* current;
 private:
 	void fillEPGList();
-	void entrySelected(eListboxEntry *entry);
-	void closeWnd();
+	void entrySelected(eListBoxEntryEPG *entry);
 public:
 	eEPGWindow(eService* service);
 	inline ~eEPGWindow(){};
