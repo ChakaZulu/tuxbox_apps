bd7e100e8f2bc34752b9833f423492e70d078cb1
Ghostrider <Ghostrider>
2002-06-03 11:37:29 +0000

    improved qsocket and qserversocket for libsigc... temporaray...

---
+++ tuxbox/enigma/lib/socket/qserversocket.h
@@ -0,0 +1,82 @@
+/****************************************************************************
+** $Id: qserversocket.h,v 1.1 2002/06/03 11:37:29 Ghostrider Exp $
+**
+** Definition of QServerSocketClass.
+**
+** Created : 970521
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of the network module of the Qt GUI Toolkit.
+**
+** This file may be distributed under the terms of the Q Public License
+** as defined by Trolltech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** Licensees holding valid Qt Enterprise Edition licenses may use this
+** file in accordance with the Qt Commercial License Agreement provided
+** with the Software.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+**   information about Qt Commercial License Agreements.
+** See http://www.trolltech.com/qpl/ for QPL licensing information.
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
+#ifndef QSERVERSOCKET_H
+#define QSERVERSOCKET_H
+
+#ifndef QT_H
+#include <include/libsig_comp.h>
+#include <core/socket/qsocket.h>
+#include "qobject.h"
+#endif // QT_H
+#ifndef QT_NO_NETWORK
+
+class QServerSocketPrivate;
+
+class Q_EXPORT QServerSocket : public QObject, public Object
+{
+    Q_OBJECT
+public:
+    QServerSocket( Q_UINT16 port, int backlog = 0 );
+    QServerSocket( const QHostAddress & address, Q_UINT16 port, int backlog = 0);
+    QServerSocket( );
+    virtual ~QServerSocket();
+
+    bool ok() const;
+
+    Q_UINT16 port() const ;
+
+    int socket() const ;
+    virtual void setSocket( int socket );
+
+    QHostAddress address() const ;
+
+    virtual void newConnection( int socket ) = 0;
+
+protected:
+    QSocketDevice *socketDevice();
+
+private slots:
+    void incomingConnection( int socket );
+
+private:
+    QServerSocketPrivate *d;
+    void init( const QHostAddress & address, Q_UINT16 port, int backlog );
+};
+
+#endif // QT_NO_NETWORK
+#endif // QSERVERSOCKET_H
