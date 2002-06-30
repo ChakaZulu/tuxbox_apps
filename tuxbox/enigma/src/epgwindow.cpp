17f4b89f4821eb8638119fb347b175f324fd6b1f
tmbinc <tmbinc>
2002-06-30 01:25:23 +0000

    warning

--- tuxbox/enigma/src/epgwindow.cpp
+++ tuxbox/enigma/src/epgwindow.cpp
@@ -78,7 +78,7 @@ void eEPGWindow::entrySelected(eListBoxEntryEPG *entry)
 }
 
 eEPGWindow::eEPGWindow(eService* service)
-:current(service),eListBoxWindow<eListBoxEntryEPG>("Select Service...", 16, eSkin::getActive()->queryValue("fontsize", 20), 600)
+	:eListBoxWindow<eListBoxEntryEPG>("Select Service...", 16, eSkin::getActive()->queryValue("fontsize", 20), 600), current(service)
 {
 	move(ePoint(50, 50));
 	list.setActiveColor(eSkin::getActive()->queryScheme("eServiceSelector.highlight"));
