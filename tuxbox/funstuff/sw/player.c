10ecb6323c71e7d9bfb02d5374946ae391542ad0
obi <obi>
2002-08-21 08:59:42 +0000

    no more compile warnigns

--- tuxbox/funstuff/sw/player.c
+++ tuxbox/funstuff/sw/player.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
  
  if((fd=popen("gunzip -c sw1.txt.gz","r")) == NULL) {
   fprintf(stderr,"pipe-error executing gunzip -c sw1.txt.gz\n");
-  exit(1);
+  return 1;
  }
 
  while(fgets(buffer,255,fd) != NULL) {
