a1b73d4e0549d4db30b46176bc2d6e9b138e58c3
thegoodguy <thegoodguy>
2002-09-17 13:31:09 +0000

    Speed up saving/commiting bouquet changes (ATTENTION: libzapitclient change - additional feature) - Part II

--- tuxbox/neutrino/src/gui/bedit/bouqueteditor_bouquets.cpp
+++ tuxbox/neutrino/src/gui/bedit/bouqueteditor_bouquets.cpp
@@ -518,11 +518,9 @@ void CBEBouquetWidget::saveChanges()
 	CHintBox* hintBox= new CHintBox( "bouqueteditor.name", g_Locale->getText("bouqueteditor.savingchanges"), "info.raw", 480 );
 	hintBox->paint();
 	g_Zapit->saveBouquets();
-	g_Zapit->reinitChannels();  // reload services & bouquets
+	g_Zapit->commitBouquetChange();
 	hintBox->hide();
 	delete hintBox;
-
-//	g_RCInput->postMsg( NeutrinoMessages::EVT_BOUQUETSCHANGED, 0 );  // event send by zapit
 }
 
 void CBEBouquetWidget::discardChanges()
