8a8a41f154dc130f8704f844868a7c96be87aaad
thegoodguy <thegoodguy>
2002-09-19 10:39:09 +0000

    Add GNU GPL Header

--- dvb/zapit/src/xmlinterface.cpp
+++ dvb/zapit/src/xmlinterface.cpp
@@ -1,5 +1,24 @@
 /*
- * $Header: /cvs/tuxbox/apps/dvb/zapit/src/Attic/xmlinterface.cpp,v 1.2 2002/09/19 10:24:50 thegoodguy Exp $
+ * $Header: /cvs/tuxbox/apps/dvb/zapit/src/Attic/xmlinterface.cpp,v 1.3 2002/09/19 10:39:09 thegoodguy Exp $
+ *
+ * xmlinterface for zapit - d-box2 linux project
+ *
+ * (C) 2002 by thegoodguy <thegoodguy@berlios.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 #include "xmlinterface.h"
