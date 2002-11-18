47da481234c9d40737ce75b47052a166fa6a4af4
obi <obi>
2002-11-18 00:38:30 +0000

    - added debug.h which contains some macros   for debug/warning/info output.

---
+++ dvb/zapit/include/zapit/debug.h
@@ -0,0 +1,88 @@
+/*
+ * $Id: debug.h,v 1.1 2002/11/18 00:38:30 obi Exp $
+ *
+ * (C) 2002 by Andreas Oberritter <obi@tuxbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __zapit_debug_h__
+#define __zapit_debug_h__
+
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#else
+#define DEBUG	1
+#endif
+
+
+/* zapit.cpp */
+extern int debug;
+
+
+#define DBG(fmt, args...)					\
+	do {							\
+		if (debug)					\
+			fprintf(stdout, "[%s:%s:%d] " fmt "\n",	\
+				__FILE__, __FUNCTION__,		\
+				__LINE__, ## args);		\
+	} while (0)
+
+
+#define ERROR(str)						\
+	do {							\
+		fprintf(stderr, "[%s:%s:%d] %s: %s\n",		\
+			__FILE__, __FUNCTION__,			\
+			__LINE__, str, strerror(errno));	\
+	} while (0)
+
+
+#ifdef DEBUG
+
+
+#define INFO(fmt, args...)					\
+	do {							\
+		fprintf(stdout, "[%s:%s:%d] " fmt "\n",		\
+			__FILE__, __FUNCTION__,			\
+			__LINE__, ## args);			\
+	} while (0)
+
+
+#define WARN(fmt, args...)					\
+	do {							\
+		fprintf(stderr, "[%s:%s:%d] " fmt "\n",		\
+			__FILE__, __FUNCTION__,			\
+			__LINE__, ## args);			\
+	} while (0)
+
+
+#else /* DEBUG */
+
+
+#define INFO(fmt, args...)
+#define WARN(fmt, args...)
+
+
+#endif /* DEBUG */
+
+
+#endif /* __zapit_debug_h__ */
