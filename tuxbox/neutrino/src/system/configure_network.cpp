a1899a4a2600f81ef4cf1da84c45534c89794f6f
thegoodguy <thegoodguy>
2003-03-20 14:17:26 +0000

    Fix writing of loopback device

--- tuxbox/neutrino/src/system/configure_network.cpp
+++ tuxbox/neutrino/src/system/configure_network.cpp
@@ -1,5 +1,5 @@
 /*
- * $Header: /cvs/tuxbox/apps/tuxbox/neutrino/src/system/configure_network.cpp,v 1.4 2003/03/20 14:08:10 thegoodguy Exp $
+ * $Header: /cvs/tuxbox/apps/tuxbox/neutrino/src/system/configure_network.cpp,v 1.5 2003/03/20 14:17:26 thegoodguy Exp $
  *
  * (C) 2003 by thegoodguy <thegoodguy@berlios.de>
  *
@@ -64,12 +64,12 @@ void CNetworkConfig::commitConfig(void)
 		if (inet_static)
 		{
 			netSetNameserver(nameserver.c_str());
-			addLoopbackDevice("eth0", true);
+			addLoopbackDevice("lo", true);
 			setStaticAttributes("eth0", automatic_start, address, netmask, broadcast, gateway);
 		}
 		else
 		{
-			addLoopbackDevice("eth0", true);
+			addLoopbackDevice("lo", true);
 			setDhcpAttributes("eth0", automatic_start);
 		}
 	}
