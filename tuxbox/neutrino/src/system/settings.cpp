3583d49daff8d4c5362a53f2b837b7c3367af9ff
digi_casi <digi_casi>
2003-05-24 14:15:50 +0000

    remove warning

--- tuxbox/neutrino/src/system/settings.cpp
+++ tuxbox/neutrino/src/system/settings.cpp
@@ -1,6 +1,6 @@
 /*
 
-        $Id: settings.cpp,v 1.25 2003/05/22 20:37:12 digi_casi Exp $
+        $Id: settings.cpp,v 1.26 2003/05/24 14:15:50 digi_casi Exp $
 
 	Neutrino-GUI  -   DBoxII-Project
 
@@ -213,7 +213,6 @@ bool CScanSettings::saveSettings(const std::string fileName)
 	int satCount = 0;
 	char tmp[20] = "";
 	int i = 0;
-	FILE * fd = NULL;
 	
 	configfile.setInt32("delivery_system", delivery_system);
 	configfile.setInt32( "diseqcMode", diseqcMode );
