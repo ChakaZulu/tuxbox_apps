7a1738e25cbe943dd90caea3a88ea5cd23de7f14
carjay <carjay>
2003-09-22 18:43:43 +0000

    updated for event input device, also some changes to the code, see readme for details

--- tuxbox/lcd/lcdip/lcdip.cpp
+++ tuxbox/lcd/lcdip/lcdip.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <errno.h>
+#include <linux/input.h>
+
 #include "lcddisplay.h"
 #include "rc.h"
 
@@ -8,16 +11,27 @@ int pt[3]={1, 10, 100};
 
 int main()
 {
+	int ip[4]={192,168,0,1};
+
 	display.draw_string(10, 10, "IP eingeben:");
-	FILE* pFile=fopen("/var/lcdip", "r"); // tum lesen "w" zum schreiben
-	int update=0;
-	update=1;
 	
+	FILE* pFile=fopen("/var/ip.lcdip", "r"); // tum lesen "w" zum schreiben
+	if ((pFile == NULL)&&(errno != ENOENT)){
+		printf ("error: %d ",errno);
+		perror ("/var/ip.lcdip");
+		return 1;
+	} 
+	else if (pFile != NULL){
+		int err = fscanf (pFile,"%03d.%03d.%03d.%03d\n",&ip[0],&ip[1],&ip[2],&ip[3]);
+		if ((err == EOF) ||( err<4)){
+			printf ("error: /var/ip.lcdip too short, using default\n");
+			ip[0]=192;ip[1]=168;ip[2]=0;ip[3]=1;
+		}
+		fclose(pFile);
+	}
+
 	int curpos=0;
-	//int ip[4]={192, 168, 0, 202};
-	int ip[4];
-	for(int i=0; i< 4; i++) ip[i]=fgetc(pFile);
-	fclose(pFile);
+	int update=1;
 	int done=0;
 	RCOpen();
 	while (!done)
@@ -27,21 +41,21 @@ int main()
 			int code;
 			switch (code=RCGet())
 			{
-			case RC_LEFT:
+			case KEY_LEFT:
 				if (curpos)
 				{
 					curpos--;
 					update=1;
 				}
 				break;
-			case RC_RIGHT:
+			case KEY_RIGHT:
 				if (curpos<14)
 				{
 					curpos++;
 					update=1;
 				}
 				break;
-			case RC_UP:
+			case KEY_UP:
 			{
 				if ((curpos%4)==3)
 					break;
@@ -58,8 +72,21 @@ int main()
 				update=1;
 				break;
 			}
-			case RC_0...RC_9:
+
+			case KEY_0:
+			case KEY_1:
+			case KEY_2:
+			case KEY_3:
+			case KEY_4:
+			case KEY_5:
+			case KEY_6:
+			case KEY_7:
+			case KEY_8:
+			case KEY_9:
 			{
+				unsigned char keynumber[10]={KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9};
+				int number;
+				for (number=0;number<10;number++) if (code==keynumber[number]) break;
 				if ((curpos%4)==3)
 					break;
 				int i=curpos/4;
@@ -67,7 +94,7 @@ int main()
 				int nv=ip[i];
 				int s=(nv/pt[a])%10;
 				nv-=s*pt[a];
-				s=code;
+				s=number;
 				nv+=s*pt[a];
 				ip[i]=nv; 
 				update=1;
@@ -80,7 +107,7 @@ int main()
 					done=1;*/ 
 				break;
 			}
-			case RC_DOWN:
+			case KEY_DOWN:
 			{
 				if ((curpos%4)==3)
 					break;
@@ -97,7 +124,13 @@ int main()
 				update=1;
 				break;
 			}
-			case RC_OK:
+			case KEY_POWER:
+				if (curpos<14){
+					curpos++;
+					update=1;
+					break;
+				}
+			case KEY_OK:
 				done=1;
 				update=1;
 				break;
@@ -112,9 +145,11 @@ int main()
 				if ((ip[0]>255) || (ip[1]>255) || (ip[2]>255) || (ip[3]>255))
 				{
 					done=0;
-					display.draw_string(0, 30, " UNGÜLTIGE IP! ");
+					display.draw_string(0, 30, "  INVALID IP!  ");
+					curpos = 0;
 					display.update();
-					while (RCGet()!=RC_OK);
+					int code = RCGet();
+					while ((code!=KEY_OK)&&(code!=KEY_POWER));
 				}
 			}			
 			display.draw_string(0, 30, ips);
@@ -126,12 +161,18 @@ int main()
 	}
 	RCClose();
 	
-	char exec[100];
-	sprintf(exec, "/sbin/ifconfig eth0 %d.%d.%d.%d up", ip[0], ip[1], ip[2], ip[3]);
-	FILE* paFile=fopen("/var/lcdip", "w"); 
-	for(int i=0; i< 4; i++) putc(ip[i], paFile);
+	FILE* paFile=fopen("/var/ip.lcdip", "w"); 
+	if (paFile == NULL){
+		perror ("writing /var/ip.lcdip");
+		return 2;
+	}
+
+	fprintf (paFile,"%03d.%03d.%03d.%03d\n",ip[0],ip[1],ip[2],ip[3]);
 	fclose(paFile);
 
+// if needed: replace with whatever you want to do with the ip...
+/*	char exec[100];
+	sprintf(exec, "/sbin/ifconfig eth0 %d.%d.%d.%d up", ip[0], ip[1], ip[2], ip[3]);
 	if (!(system(exec)>>8))
 	{
 		display.draw_string(0, 30, "  IP GESETZT!  ");
@@ -141,5 +182,8 @@ int main()
 		display.draw_string(0, 30, "    FEHLER!    ");
 		display.update();
 	}
+*/
+	display.draw_string(0, 30, "    IP OK!     ");
+	display.update();
 	return 0;
 }
--- tuxbox/lcd/lcdip/rc.cpp
+++ tuxbox/lcd/lcdip/rc.cpp
@@ -2,62 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <linux/input.h>
+
 #include "rc.h"
 
 int fd=-1;
-
-static int translate(int code)
-{
-	if ((code&0xFF00)==0x5C00)
-	{
-		switch (code&0xFF)
-		{
-		case 0x0C: return RC_STANDBY;
-		case 0x20: return RC_HOME;
-		case 0x27: return RC_DBOX;
-		case 0x00: return RC_0;
-		case 0x01: return RC_1;
-		case 0x02: return RC_2;
-		case 0x03: return RC_3;
-		case 0x04: return RC_4;
-		case 0x05: return RC_5;
-		case 0x06: return RC_6;
-		case 0x07: return RC_7;
-		case 0x08: return RC_8;
-		case 0x09: return RC_9;
-		case 0x3B: return RC_BLUE;
-		case 0x52: return RC_YELLOW;
-		case 0x55: return RC_GREEN;
-		case 0x2D: return RC_RED;
-		case 0x54: return RC_UP_LEFT;
-		case 0x53: return RC_DOWN_LEFT;
-		case 0x0E: return RC_UP;
- 		case 0x0F: return RC_DOWN;
-		case 0x2F: return RC_LEFT;
- 		case 0x2E: return RC_RIGHT;
-		case 0x30: return RC_OK;
- 		case 0x16: return RC_PLUS;
- 		case 0x17: return RC_MINUS;
- 		case 0x28: return RC_MUTE;
- 		case 0x82: return RC_HELP;
-		default:
-			printf("unknown old rc code %x\n", code);
-			return -1;
-		}
-	} else if (!(code&0x00))
-		return code&0x3F;
-	else
-		printf("unknown rc code %x\n", code);
-	return -1;
-}
-
+int lastpressed = KEY_UNKNOWN;
 
 int RCOpen()
 {
-  fd=open("/dev/dbox/rc0", O_RDONLY);
+  fd=open("/dev/input/event0", O_RDONLY);
   if (fd<0)
   {
-    perror("/dev/dbox/rc0");
+    perror("/dev/input/event0");
     return fd;
   }
   return 0;
@@ -65,10 +22,28 @@ int RCOpen()
 
 int RCGet()
 {
-  unsigned short rc;
-  if (read(fd, &rc, 2)!=2)
-    return -1;
-  return translate(rc);
+	struct input_event rc_ev;
+	int rd;
+	unsigned char new_key = 0;
+	do {
+		rd = read(fd, &rc_ev, sizeof(struct input_event));
+		if (rc_ev.value == !0){
+			if (rc_ev.code != lastpressed){	// new button was pressed
+				lastpressed = rc_ev.code;
+				new_key = 1;
+			}
+		} 
+		else if (rc_ev.value == 0){	// (some) button was released, accept same key again
+			lastpressed = KEY_UNKNOWN;
+		}
+	}
+	while(!new_key);
+
+	if (rd < (int) sizeof(struct input_event)){
+		perror("RCGet()");
+		return -1;
+ 	}
+	return rc_ev.code;
 }
 
 int RCPoll()
