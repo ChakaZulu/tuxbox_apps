574d9bdae329074c6859848dd710feb44b729223
rasc <rasc>
2003-10-19 14:00:43 +0000

    -more table decoding

--- dvb/dvbsnoop/src/version.h
+++ dvb/dvbsnoop/src/version.h
@@ -1,11 +1,14 @@
 /*
-$Id: version.h,v 1.1 2003/10/17 19:14:40 rasc Exp $
+$Id: version.h,v 1.2 2003/10/19 14:00:43 rasc Exp $
 
   dvbsnoop
   (c) rasc
 
 
 $Log: version.h,v $
+Revision 1.2  2003/10/19 14:00:43  rasc
+-more table decoding
+
 Revision 1.1  2003/10/17 19:14:40  rasc
 no message
 
@@ -20,5 +23,5 @@ no message
  * Version of dvbsnoop
  * */
 
-#define DVBSNOOP_VERSION  "1.0c"
+#define DVBSNOOP_VERSION  "1.0.1"
 
