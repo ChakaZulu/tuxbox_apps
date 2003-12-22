26df3365c2e7f79b7b5f81b3f245ba840a1f0dc0
sepp776 <sepp776>
2003-12-22 22:06:48 +0000

    Fix last commit

--- tuxbox/neutrino/src/system/setting_helpers.cpp
+++ tuxbox/neutrino/src/system/setting_helpers.cpp
@@ -390,9 +390,10 @@ bool CAudioSetupNotifier2::changeNotify(const std::string & OptionName, void*)
 
 	if (g_settings.audio_avs_Control == 2)   
 		g_Controld->setVolume(100 - atoi(g_settings.audio_PCMOffset), 0);
-	else
+	else {
 		g_Controld->setVolume(100, 0);
 		g_Controld->setVolume(100, 1);
+  }
  
 	return true;
 
