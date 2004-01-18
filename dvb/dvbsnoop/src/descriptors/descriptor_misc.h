70fc692009c8980a530ec9e053de34cd9812593a
rasc <rasc>
2004-01-18 00:30:48 +0000

    no message

---
+++ dvb/dvbsnoop/src/descriptors/descriptor_misc.h
@@ -0,0 +1,35 @@
+/*
+$Id: descriptor_misc.h,v 1.1 2004/01/18 00:30:48 rasc Exp $ 
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+ -- Descriptors  misc. routines, helpers, etc.
+
+
+$Log: descriptor_misc.h,v $
+Revision 1.1  2004/01/18 00:30:48  rasc
+no message
+
+
+
+
+*/
+
+
+#ifndef _DESCRIPTOR_MISC_H
+#define _DESCRIPTOR_MISC_H 1
+
+
+void  out_nl_calc_NPT (int v, unsigned long long npt);
+
+
+#endif
+
+
