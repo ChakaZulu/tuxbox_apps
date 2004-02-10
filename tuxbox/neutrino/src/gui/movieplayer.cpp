3069a4e0a6bab6e38b05b724517ec8e66fe01c03
gagga <gagga>
2004-02-10 16:59:50 +0000

    Bookmarks get correctly saved now

--- tuxbox/neutrino/src/gui/movieplayer.cpp
+++ tuxbox/neutrino/src/gui/movieplayer.cpp
@@ -4,7 +4,7 @@
   Movieplayer (c) 2003, 2004 by gagga
   Based on code by Dirch, obi and the Metzler Bros. Thanks.
 
-  $Id: movieplayer.cpp,v 1.76 2004/02/10 15:41:48 gagga Exp $
+  $Id: movieplayer.cpp,v 1.77 2004/02/10 16:59:50 gagga Exp $
 
   Homepage: http://www.giggo.de/dbox2/movieplayer.html
 
@@ -163,6 +163,7 @@ CMoviePlayerGui::CMoviePlayerGui()
 CMoviePlayerGui::~CMoviePlayerGui ()
 {
 	delete filebrowser;
+	delete bookmarkmanager;
 	g_Zapit->setStandby (false);
 	g_Sectionsd->setPauseScanning (false);
 
@@ -185,7 +186,10 @@ CMoviePlayerGui::exec (CMenuTarget * parent, const std::string & actionKey)
     if (actionKey=="bookmarkplayback") {
         isBookmark = true;
         theBookmark = bookmarkmanager->getBookmark(NULL);
-        if (theBookmark == NULL) return menu_return::RETURN_REPAINT;
+        if (theBookmark == NULL) {
+            bookmarkmanager->flush();
+            return menu_return::RETURN_REPAINT;
+        }
 	}
 	
 	
@@ -1573,7 +1577,7 @@ CMoviePlayerGui::PlayStream (int streamtype)
 		else if (msg == CRCInput::RC_help)
  		{
      		std::string helptext = g_Locale->getText("movieplayer.vlchelp");
-     		std::string fullhelptext = helptext + "\nVersion: $Revision: 1.76 $\n\nMovieplayer (c) 2003, 2004 by gagga";
+     		std::string fullhelptext = helptext + "\nVersion: $Revision: 1.77 $\n\nMovieplayer (c) 2003, 2004 by gagga";
      		ShowMsgUTF("messagebox.info", fullhelptext.c_str(), CMessageBox::mbrBack, CMessageBox::mbBack, "info.raw"); // UTF-8
  		}
 		else
@@ -1745,7 +1749,7 @@ CMoviePlayerGui::PlayFile (void)
  		else if (msg == CRCInput::RC_help)
  		{
 			std::string fullhelptext = g_Locale->getText("movieplayer.tshelp");
-			fullhelptext += "\nVersion: $Revision: 1.76 $\n\nMovieplayer (c) 2003, 2004 by gagga";
+			fullhelptext += "\nVersion: $Revision: 1.77 $\n\nMovieplayer (c) 2003, 2004 by gagga";
 			ShowMsgUTF("messagebox.info", fullhelptext.c_str(), CMessageBox::mbrBack, CMessageBox::mbBack, "info.raw"); // UTF-8
  		}
  		else if (msg == CRCInput::RC_setup)
