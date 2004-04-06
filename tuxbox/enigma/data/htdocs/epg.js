a07e738db08723e40520ccbd1afd469033bafecb
digi_casi <digi_casi>
2004-04-06 09:30:21 +0000

    add switch channel capability for webif multi-epg

--- tuxbox/enigma/data/htdocs/epg.js
+++ tuxbox/enigma/data/htdocs/epg.js
@@ -19,3 +19,10 @@ function EPGDetails(xy)
 {
 	NewWindow('/EPGDetails?'+xy, 'EPGDetails', '600', '700', 'no');
 }
+
+function switchChannel(xy)
+{
+	win=window.open("?path="+xy, "switchStatus","width=1,height=1,left=0,top=0");
+	win.focus();
+	parent.setTimeout("reload()", 3000);
+}
