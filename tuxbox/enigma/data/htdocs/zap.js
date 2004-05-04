f1fcc7fed8273dc082d95394b1b98f839919e782
digi_casi <digi_casi>
2004-05-04 09:01:16 +0000

    webif: prep for new zapping interface

---
+++ tuxbox/enigma/data/htdocs/zap.js
@@ -0,0 +1,62 @@
+function channelChange()
+{
+	var channel = "";
+	var j = document.channelselector.channel.selectedIndex;
+	if (j >= 0)
+	{
+		channel = document.channelselector.channel.options[j].value;
+		alert("Switch to: " + channel);
+	}
+}
+function bouquetChange()
+{
+	var i = document.channelselector.bouquet.selectedIndex;
+	loadChannels(i);
+}
+function epg()
+{
+	var channel = "";
+	var j = document.channelselector.channel.selectedIndex;
+	if (j >= 0)
+	{
+		channel = document.channelselector.channel.options[j].value;
+		alert("EPG for: " + channel);
+	}
+	else
+		alert("No Channel selected");
+}
+function mepg()
+{
+	var i = document.channelselector.bouquet.selectedIndex;
+	var bouquet = document.channelselector.bouquet.options[i].value;
+	alert("Multi-EPG for: " + bouquet);
+}
+function loadChannels(bouquet)
+{
+	deleteChannelOptions();
+	addChannelOptions(bouquet);
+	document.channelselector.channel.selectedIndex = -1;
+}
+function addChannelOptions(bouquet)
+{
+	for (var i = 0; i < channels[bouquet].length; i++)
+	{
+		newOption = new Option(channels[bouquet][i], channelRefs[bouquet][i], false, true);
+		document.channelselector.channel.options[document.channelselector.channel.length] = newOption;
+	}
+}
+function deleteChannelOptions()
+{
+	var j = document.channelselector.channel.options.length;
+	for (var i = j - 1 ; i >= 0; i--)
+		document.channelselector.channel.options[i] = null;
+}
+function loadBouquets()
+{
+	for (var i = 0; i < bouquets.length; i++)
+	{
+		newOption = new Option(bouquets[i], bouquetRefs[i], false, true);
+		document.channelselector.bouquet.options[i] = newOption;
+	}
+	document.channelselector.bouquet.selectedIndex = 0;
+}
---
+++ tuxbox/enigma/data/htdocs/zapdata.js
@@ -0,0 +1,17 @@
+var channels = new Array();
+var channelRefs = new Array();
+var bouquets = new Array(
+		#BOUQUETS#
+		);
+var bouquetRefs = new Array(
+		#BOUQUETREFS#
+		);
+			
+function init()
+{	
+	#CHANNELS#
+	);
+					
+	loadBouquets();
+	loadChannels(0);
+}
