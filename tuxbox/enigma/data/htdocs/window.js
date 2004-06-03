de43cfdcd8f46c326a952ebdf55d5b3cb19690a0
digi_casi <digi_casi>
2004-06-03 20:28:06 +0000

    webif: display pda windows just half the size of the regular ones...

--- tuxbox/enigma/data/htdocs/window.js
+++ tuxbox/enigma/data/htdocs/window.js
@@ -1,11 +1,18 @@
 function NewWindow(mypage, myname, w, h, scroll, timeout)
 {
+	if (screen.width < 800)
+	{
+		w = w / 2;
+		h = h / 2;
+	}
 	var winl = (screen.width - w) / 2;
 	var wint = (screen.height - h) / 2;
 	winprops = 'height='+h+', width='+w+', top='+wint+', left='+winl+', scrollbars='+scroll+', resizable'
 	win = window.open(mypage, myname, winprops)
-	if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
-	if (timeout > 0) { win.window.setTimeout("close()", timeout); }
+	if (parseInt(navigator.appVersion) >= 4)
+		win.window.focus();
+	if (timeout > 0)
+		win.window.setTimeout("close()", timeout);
 }
 
 function reload()
