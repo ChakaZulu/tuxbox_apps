7f067079b12f966e6c9fc4410e18e54bd01efacb
obi <obi>
2004-06-17 09:54:20 +0000

    reverted commit by Marcel: use getTableIdExtension() instead

--- dvb/libdvbsi++/include/dvbsi++/program_map_section.h
+++ dvb/libdvbsi++/include/dvbsi++/program_map_section.h
@@ -1,5 +1,5 @@
 /*
- * $Id: program_map_section.h,v 1.2 2004/06/17 00:06:00 mws Exp $
+ * $Id: program_map_section.h,v 1.3 2004/06/17 09:54:20 obi Exp $
  *
  * Copyright (C) 2002-2004 Andreas Oberritter <obi@saftware.de>
  *
@@ -49,7 +49,6 @@ typedef ElementaryStreamInfoVector::const_iterator ElementaryStreamInfoConstIter
 class ProgramMapSection : public LongCrcSection, public DescriptorContainer
 {
 	protected:
-		unsigned programNumber			: 16;
 		unsigned pcrPid					: 13;
 		unsigned programInfoLength			: 12;
 		ElementaryStreamInfoVector esInfo;
@@ -61,7 +60,6 @@ class ProgramMapSection : public LongCrcSection, public DescriptorContainer
 		static const enum TableId TID = TID_PMT;
 		static const uint32_t TIMEOUT = 600;
 
-		uint16_t getProgramNumber(void) const;
 		uint16_t getPcrPid(void) const;
 		const ElementaryStreamInfoVector *getEsInfo(void) const;
 
