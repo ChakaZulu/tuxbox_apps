9deb632eaeba7a1a7514d38f6721a33d1072011c
digi_casi <digi_casi>
2004-09-29 20:31:38 +0000

    webif: remove some popup windows for pdas

--- tuxbox/enigma/data/htdocs/window.js
+++ tuxbox/enigma/data/htdocs/window.js
@@ -1,11 +1,18 @@
 function NewWindow(mypage, myname, w, h, scroll, timeout)
 {
-	var winl = (screen.width - w) / 2;
-	var wint = (screen.height - h) / 2;
-	winprops = 'height='+h+', width='+w+', top='+wint+', left='+winl+', scrollbars='+scroll+', resizable'
-	win = window.open(mypage, myname, winprops)
-	if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
-	if (timeout > 0) { win.window.setTimeout("close()", timeout); }
+	if (screen.width >= 800)
+	{
+		var winl = (screen.width - w) / 2;
+		var wint = (screen.height - h) / 2;
+		winprops = 'height='+h+', width='+w+', top='+wint+', left='+winl+', scrollbars='+scroll+', resizable'
+		win = window.open(mypage, myname, winprops)
+		if (parseInt(navigator.appVersion) >= 4)
+			win.window.focus();
+		if (timeout > 0)
+			win.window.setTimeout("close()", timeout);
+	}
+	else
+		document.location = mypage;
 }
 
 function reload()
