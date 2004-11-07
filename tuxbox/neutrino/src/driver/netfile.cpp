0e894f1f6d373a09b6a5595ad0c4288b87175dfe
thegoodguy <thegoodguy>
2004-11-07 11:12:12 +0000

    fix 32 bit integer operation

--- tuxbox/neutrino/src/driver/netfile.cpp
+++ tuxbox/neutrino/src/driver/netfile.cpp
@@ -1013,7 +1013,7 @@ FILE *f_open(const char *filename, const char *acctype)
 	  /* magic, if there is any */
 	  for (int i = 0; i < known_magic_count; i++)
 	  {
-		  if ((((uint32_t)magic) & (uint32_t)known_magic[i].mask) == (uint32_t)known_magic[i].mode)
+		  if (((*(uint32_t *)&(magic[0])) & *(uint32_t *)&(known_magic[i].mask[0])) == *(uint32_t *)&(known_magic[i].mode[0]))
 		  {
 			  f_type(fd, (char *)known_magic[i].type);
 			  goto magic_found;
