2cf63dc8347318dd829b1656762601e3ef11f6a1
digi_casi <digi_casi>
2004-11-16 17:42:31 +0000

    skelleton for streaming manager

---
+++ tuxbox/enigma/src/enigma_streamer.h
@@ -0,0 +1,20 @@
+#ifndef __enigma_streamer__
+#define __enigma_streamer__
+
+#include <lib/base/estring.h>
+#include <lib/dvb/dvb.h>
+
+class eStreamer
+{
+private:
+	static eStreamer *instance;
+	eServiceReference sref;
+public:
+	eStreamer();
+	~eStreamer();
+
+	bool getServiceReference(eServiceReference&);
+	void setServiceReference(eServiceReference);
+};
+#endif
+
