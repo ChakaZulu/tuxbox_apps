3188ebe5cf766db0db9f66dfb4d3df2ffd3a4663
digi_casi <digi_casi>
2004-12-22 11:01:21 +0000

    nicer

--- tuxbox/enigma/lib/picviewer/png.cpp
+++ tuxbox/enigma/lib/picviewer/png.cpp
@@ -4,8 +4,8 @@
 	#include <png.h>
 	#include <sys/types.h>
 	#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+	#include <fcntl.h>
+	#include <unistd.h>
 
 	#include "pictureviewer.h"
 
@@ -16,15 +16,18 @@ int fh_png_id(const char *name)
 {
 	int fd;
 	char id[4];
-	fd=open(name,O_RDONLY); if(fd==-1) return(0);
-	read(fd,id,4);
+	fd = open(name, O_RDONLY); 
+	if (fd == -1) 
+		return(0);
+	read(fd, id, 4);
 	close(fd);
-	if(id[1]=='P' && id[2]=='N' && id[3]=='G') return(1);
+	if (id[1] == 'P' && id[2] == 'N' && id[3] == 'G') 
+		return(1);
 	return(0);
 }
 
 
-int fh_png_load(const char *name,unsigned char *buffer,int x,int y)
+int fh_png_load(const char *name, unsigned char *buffer, int x, int y)
 {
 	static const png_color_16 my_background = {0, 0, 0, 0, 0};
 
@@ -35,23 +38,27 @@ int fh_png_load(const char *name,unsigned char *buffer,int x,int y)
 	int bit_depth, color_type, interlace_type;
 	int number_passes,pass;
 	png_byte * fbptr;
-	FILE     * fh;
+	FILE * fh;
 
-	if(!(fh=fopen(name,"rb")))	return(FH_ERROR_FILE);
+	if (!(fh = fopen(name,"rb")))
+		return(FH_ERROR_FILE);
 
 	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
-	if(png_ptr == NULL) return(FH_ERROR_FORMAT);
+	if (png_ptr == NULL) 
+		return(FH_ERROR_FORMAT);
 	info_ptr = png_create_info_struct(png_ptr);
-	if(info_ptr == NULL)
+	if (info_ptr == NULL)
 	{
 		png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-		fclose(fh); return(FH_ERROR_FORMAT);
+		fclose(fh); 
+		return(FH_ERROR_FORMAT);
 	}
 
-	if(setjmp(png_ptr->jmpbuf))
+	if (setjmp(png_ptr->jmpbuf))
 	{
 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-		fclose(fh); return(FH_ERROR_FORMAT);
+		fclose(fh); 
+		return(FH_ERROR_FORMAT);
 	}
 
 	png_init_io(png_ptr,fh);
@@ -117,29 +124,33 @@ int fh_png_getsize(const char *name,int *x,int *y, int wanted_width, int wanted_
 	int bit_depth, color_type, interlace_type;
 	FILE *fh;
 
-	if(!(fh=fopen(name,"rb")))	return(FH_ERROR_FILE);
+	if (!(fh = fopen(name,"rb")))	
+		return(FH_ERROR_FILE);
 
 	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
-	if(png_ptr == NULL) return(FH_ERROR_FORMAT);
+	if (png_ptr == NULL) 
+		return(FH_ERROR_FORMAT);
 	info_ptr = png_create_info_struct(png_ptr);
-	if(info_ptr == NULL)
+	if (info_ptr == NULL)
 	{
 		png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-		fclose(fh); return(FH_ERROR_FORMAT);
+		fclose(fh); 
+		return(FH_ERROR_FORMAT);
 	}
 
-	if(setjmp(png_ptr->jmpbuf))
+	if (setjmp(png_ptr->jmpbuf))
 	{
 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-		fclose(fh); return(FH_ERROR_FORMAT);
+		fclose(fh); 
+		return(FH_ERROR_FORMAT);
 	}
 
 	png_init_io(png_ptr,fh);
 	png_read_info(png_ptr, info_ptr);
 	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,&interlace_type, NULL, NULL);
 	png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-	*x=width;
-	*y=height;
+	*x = width;
+	*y = height;
 	fclose(fh);
 	return(FH_ERROR_OK);
 }
