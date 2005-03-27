c18c0076b249fe680630644c5a20c6d4b3b82063
zwen <zwen>
2005-03-27 22:26:00 +0000

    fix & cleanup

--- tuxbox/neutrino/src/driver/pictureviewer/make15color_errdiff.cpp
+++ tuxbox/neutrino/src/driver/pictureviewer/make15color_errdiff.cpp
@@ -37,40 +37,46 @@ void c32_15(unsigned char r, unsigned char g , unsigned char b , unsigned char* 
 		*(d+1) = ((g << 2) & 0xE0) | (b >> 3);
 }
 
-#define FS_CALC_ERROR(color, index) \
-				p1 = p2 = (ptr[index] + (this_line_error_##color[ix]>>4)); \
+#define FS_CALC_ERROR_COMMON(color, index) \
+				p1 = p2 = (p_src[index] + (this_line_error_##color[ix]>>4)); \
 				if(p1>255)p1=255; if(p1<0)p1=0; \
 				color = (p1 & 0xF8) | 0x4; \
 				error = p2 - color; \
-				if(error > 255)error=255;if(error<-255)error=-255; \
+
+#define FS_CALC_ERROR_RIGHT(color, index) \
+				FS_CALC_ERROR_COMMON(color,index) \
+				this_line_error_##color[ix+1] += (error * 7); \
+				next_line_error_##color[ix-1] += (error * 3); \
+				next_line_error_##color[ix]   += (error * 5); \
+				next_line_error_##color[ix+1] += error;
+				
+#define FS_CALC_ERROR_LEFT(color, index) \
+				FS_CALC_ERROR_COMMON(color,index) \
 				this_line_error_##color[ix-1] += (error * 7); \
 				next_line_error_##color[ix+1] += (error * 3); \
 				next_line_error_##color[ix]   += (error * 5); \
 				next_line_error_##color[ix-1] += error;
 				
-
-unsigned char * make15color_errdiff(unsigned char * orgin,int x, int y)
+unsigned char * make15color_errdiff(unsigned char * src,int width, int height)
 {
 	int odd_line=1;
 	int ix,iy, error, p1, p2;
 	unsigned char r,g,b;
-	unsigned char *ptr, *dst;
-	unsigned char* np = (unsigned char*)malloc(x*y*2);
+	unsigned char *p_src, *p_dst;
+	unsigned char *dst = (unsigned char*) malloc(width*height*2);
 	int *this_line_error_r;
 	int *this_line_error_g;
 	int *this_line_error_b;
 	int *next_line_error_r;
 	int *next_line_error_g;
 	int *next_line_error_b;
-	int *save_error_r;
-	int *save_error_g;
-	int *save_error_b;
-	int *error1_r = (int*)malloc((x+2)*sizeof(int));
-	int *error1_g = (int*)malloc((x+2)*sizeof(int));
-	int *error1_b = (int*)malloc((x+2)*sizeof(int));
-	int *error2_r = (int*)malloc((x+2)*sizeof(int));
-	int *error2_g = (int*)malloc((x+2)*sizeof(int));
-	int *error2_b = (int*)malloc((x+2)*sizeof(int));
+	int *save_error;
+	int *error1_r = (int*) malloc((width+2)*sizeof(int));
+	int *error1_g = (int*) malloc((width+2)*sizeof(int));
+	int *error1_b = (int*) malloc((width+2)*sizeof(int));
+	int *error2_r = (int*) malloc((width+2)*sizeof(int));
+	int *error2_g = (int*) malloc((width+2)*sizeof(int));
+	int *error2_b = (int*) malloc((width+2)*sizeof(int));
 	
 	dbout("Start error diffusion\n");
 	
@@ -80,58 +86,58 @@ unsigned char * make15color_errdiff(unsigned char * orgin,int x, int y)
 	next_line_error_r = error2_r;
 	next_line_error_g = error2_g;
 	next_line_error_b = error2_b;
-	memset (this_line_error_r, 0 , (x+2) * sizeof(int));
-	memset (this_line_error_g, 0 , (x+2) * sizeof(int));
-	memset (this_line_error_b, 0 , (x+2) * sizeof(int));
-	memset (next_line_error_r, 0 , (x+2) * sizeof(int));
-	memset (next_line_error_g, 0 , (x+2) * sizeof(int));
-	memset (next_line_error_b, 0 , (x+2) * sizeof(int));
-	ptr = orgin;
-	dst = np;
+	memset (this_line_error_r, 0 , (width+2) * sizeof(int));
+	memset (this_line_error_g, 0 , (width+2) * sizeof(int));
+	memset (this_line_error_b, 0 , (width+2) * sizeof(int));
+	memset (next_line_error_r, 0 , (width+2) * sizeof(int));
+	memset (next_line_error_g, 0 , (width+2) * sizeof(int));
+	memset (next_line_error_b, 0 , (width+2) * sizeof(int));
+	p_src = src;
+	p_dst = dst;
 
-	for(iy=0 ; iy < y ; iy++)
+	for(iy=0 ; iy < height ; iy++)
 	{
-		save_error_r = this_line_error_r;
+		save_error = this_line_error_r;
 		this_line_error_r = next_line_error_r;
-		next_line_error_r = save_error_r;
-		save_error_g = this_line_error_g;
+		next_line_error_r = save_error;
+		save_error = this_line_error_g;
 		this_line_error_g = next_line_error_g;
-		next_line_error_g = save_error_g;
-		save_error_b = this_line_error_b;
+		next_line_error_g = save_error;
+		save_error = this_line_error_b;
 		this_line_error_b = next_line_error_b;
-		next_line_error_b = save_error_b;
-		memset (next_line_error_r, 0 , (x+2) * sizeof(int));
-		memset (next_line_error_g, 0 , (x+2) * sizeof(int));
-		memset (next_line_error_b, 0 , (x+2) * sizeof(int));
+		next_line_error_b = save_error;
+		memset (next_line_error_r, 0 , (width+2) * sizeof(int));
+		memset (next_line_error_g, 0 , (width+2) * sizeof(int));
+		memset (next_line_error_b, 0 , (width+2) * sizeof(int));
 		
 		if(odd_line)
 		{
-			for(ix=1 ; ix <= x ; ix++)
+			for(ix=1 ; ix <= width ; ix++)
 			{
-				FS_CALC_ERROR(r,0);
-				FS_CALC_ERROR(g,1);
-				FS_CALC_ERROR(b,2);
-				c32_15(r,g,b,dst);
-				ptr+=3;
-				dst+=2;
+				FS_CALC_ERROR_RIGHT(r,0);
+				FS_CALC_ERROR_RIGHT(g,1);
+				FS_CALC_ERROR_RIGHT(b,2);
+				c32_15(r,g,b,p_dst);
+				p_src+=3;
+				p_dst+=2;
 			}
 			odd_line=0;
 		}
 		else
 		{
-			ptr+=(x-1)*3;
-			dst+=(x-1)*2;
-			for(ix=x ; ix >= 1 ; ix--)
+			p_src+=(width-1)*3;
+			p_dst+=(width-1)*2;
+			for(ix=width ; ix >= 1 ; ix--)
 			{
-				FS_CALC_ERROR(r,0);
-				FS_CALC_ERROR(g,1);
-				FS_CALC_ERROR(b,2);
-				c32_15(r,g,b,dst);
-				ptr-=3;
-				dst-=2;
+				FS_CALC_ERROR_LEFT(r,0);
+				FS_CALC_ERROR_LEFT(g,1);
+				FS_CALC_ERROR_LEFT(b,2);
+				c32_15(r,g,b,p_dst);
+				p_src-=3;
+				p_dst-=2;
 			}
-			ptr+=x*3;
-			dst+=x*2;
+			p_src+=width*3;
+			p_dst+=width*2;
 			odd_line=1;
 		}
 	}
@@ -142,6 +148,6 @@ unsigned char * make15color_errdiff(unsigned char * orgin,int x, int y)
 	free(error2_g);
 	free(error2_b);
 	dbout("End error diffusion\n");
-	return np;
+	return dst;
 }
 
