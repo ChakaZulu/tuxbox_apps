77f06d372c312bbfa8577a13a62f51f22220468f
ghostrider <ghostrider>
2005-04-29 13:53:32 +0000

    fix marker moving

--- tuxbox/enigma/include/lib/dvb/dvb.h
+++ tuxbox/enigma/include/lib/dvb/dvb.h
@@ -535,7 +535,7 @@ public:
 	{
 		if (type != c.type)
 			return 0;
-		if ( type == idDVB )
+		if ( type == idDVB && !(c.flags&isMarker) && !(flags&isMarker) )
 			return (memcmp(data+1, c.data+1, sizeof(int)*7)==0) && (path == c.path);
 		return (memcmp(data, c.data, sizeof(int)*8)==0) && (path == c.path);
 	}
@@ -551,7 +551,7 @@ public:
 		if (type > c.type)
 			return 0;
 
-		int r = ( type == idDVB ) ?
+		int r = type == idDVB && !(c.flags&isMarker) && !(flags&isMarker) ?
 			memcmp(data+1, c.data+1, sizeof(int)*7) :
 			memcmp(data, c.data, sizeof(int)*8);
 
