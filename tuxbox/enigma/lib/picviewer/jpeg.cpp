cb9768e1bf44241e1a3598a93dce4db8c7ef39a1
digi_casi <digi_casi>
2005-04-30 16:06:26 +0000

    add function to save jpeg pics

--- tuxbox/enigma/lib/picviewer/jpeg.cpp
+++ tuxbox/enigma/lib/picviewer/jpeg.cpp
@@ -109,6 +109,44 @@ int fh_jpeg_load(const char *filename, unsigned char *buffer, int x, int y)
 	return(FH_ERROR_OK);
 }
 
+int fh_jpeg_save (JSAMPLE * image_buffer, char * filename, int quality, int image_height, int image_width)
+{
+ 	struct jpeg_compress_struct cinfo;
+ 	struct jpeg_error_mgr jerr;
+ 	FILE * outfile;		/* target file */
+ 	JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
+ 	int row_stride;		/* physical row width in image buffer */
+ 
+ 	cinfo.err = jpeg_std_error(&jerr);
+ 	jpeg_create_compress(&cinfo);
+ 
+ 	if ((outfile = fopen(filename, "wb")) == NULL) 
+	{
+		eDebug("can't open %s", filename);
+		return -1;
+	}
+ 	jpeg_stdio_dest(&cinfo, outfile);
+ 
+ 	cinfo.image_width = image_width; 	/* image width and height, in pixels */
+ 	cinfo.image_height = image_height;
+ 	cinfo.input_components = 3;		/* # of color components per pixel */
+ 	cinfo.in_color_space = JCS_RGB; 	/* colorspace of input image */
+ 
+ 	jpeg_set_defaults(&cinfo);
+ 	jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+ 	jpeg_start_compress(&cinfo, TRUE);
+ 	row_stride = image_width * 3;	/* JSAMPLEs per row in image_buffer */
+ 	while (cinfo.next_scanline < cinfo.image_height) 
+	{
+ 		row_pointer[0] = & image_buffer[cinfo.next_scanline * row_stride];
+ 		(void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+ 	}
+ 	jpeg_finish_compress(&cinfo);
+ 	fclose(outfile);
+ 	jpeg_destroy_compress(&cinfo);
+ 	return 0;
+}
+
 int fh_jpeg_getsize(const char *filename, int *x, int *y, int wanted_width, int wanted_height)
 {
 //	dbout("fh_jpeg_getsize {\n");
