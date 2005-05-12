889382daf0335d1107284aa5d12de9384dc947e7
ghostrider <ghostrider>
2005-05-12 10:18:33 +0000

    support for pngs with non 8bit

--- tuxbox/enigma/lib/gdi/epng.cpp
+++ tuxbox/enigma/lib/gdi/epng.cpp
@@ -2,65 +2,47 @@
 #include <stdio.h>
 #include <lib/gdi/epng.h>
 #include <unistd.h>
+#include <errno.h>
 
 gImage *loadPNG(const char *filename)
 {
 	__u8 header[8];
-	FILE *fp=fopen(filename, "rb");
-	
+	FILE *fp;
 	gImage *res=0;
+	png_structp png_ptr = 0;
+	png_infop info_ptr = 0;
+	png_infop end_info = 0;
 	
-	if (!fp)
+	if (!(fp = fopen(filename, "rb")))
 	{
-//		eDebug("couldn't open %s", filename );
+		eDebug("[ePNG] %s not found\n", filename);
 		return 0;
 	}
+
 	if (!fread(header, 8, 1, fp))
 	{
-		eDebug("couldn't read");
 		fclose(fp);
 		return 0;
 	}
+	
 	if (png_sig_cmp(header, 0, 8))
-	{
-		fclose(fp);
-		return 0;
-	}
-	png_structp png_ptr=png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
-	if (!png_ptr)
-	{
-		eDebug("no pngptr");
-		fclose(fp);
-		return 0;
-	}
-	png_infop info_ptr=png_create_info_struct(png_ptr);
-	if (!info_ptr)
-	{
-		eDebug("no info ptr");
-		png_destroy_read_struct(&png_ptr, (png_infopp)0, (png_infopp)0);
-		fclose(fp);
-		return 0;
-	}
-	png_infop end_info = png_create_info_struct(png_ptr);
-	if (!end_info)
-	{
-		eDebug("no end");
-		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-		fclose(fp);
-		return 0;
-	 }
+		goto pngerror;
+
+	if (!(png_ptr=png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0)))
+		goto pngerror;
+	
+	if (!(info_ptr=png_create_info_struct(png_ptr)))
+		goto pngerror;
+		
+	if (!(end_info=png_create_info_struct(png_ptr)))
+		goto pngerror;
+		
 	if (setjmp(png_ptr->jmpbuf))
-	{
-		eDebug("das war wohl nix");
-		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-		fclose(fp);
-		if (res)
-			delete res;
-		return 0;
-	}
+		goto pngerror;
+		
 	png_init_io(png_ptr, fp);
 	png_set_sig_bytes(png_ptr, 8);
-	png_set_invert_alpha(png_ptr);
+	// png_set_invert_alpha(png_ptr);	// has no effect on indexed images
 	png_read_info(png_ptr, info_ptr);
 	
 	png_uint_32 width, height;
@@ -69,57 +51,88 @@ gImage *loadPNG(const char *filename)
 	
 	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, 0, 0, 0);
 	
+	// convert 1,2 and 4 bpp to 8bpp images that enigma can blit
+	if (bit_depth < 8) {
+        png_set_packing(png_ptr);
+        bit_depth = 8;
+    }
+	
+	if (bit_depth > 8)
+		goto pngerror;
+		
 //	eDebug("%s: %dx%dx%d png, %d", filename, (int)width, (int)height, (int)bit_depth, color_type);
-	
-	if (color_type != 6)
+
+	switch (color_type) 
 	{
-		res=new gImage(eSize(width, height), bit_depth);
-	
-		png_bytep *rowptr=new png_bytep[height];
-	
-		for (unsigned int i=0; i<height; i++)
-			rowptr[i]=((png_byte*)(res->data))+i*res->stride;
-		png_read_rows(png_ptr, rowptr, 0, height);
-	
-		delete [] rowptr;
-	
-		if (png_get_valid(png_ptr, info_ptr, PNG_INFO_PLTE))
-		{
-			png_color *palette;
-			int num_palette;
-			png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette);
-			if (num_palette)
-				res->clut.data=new gRGB[num_palette];
-			else
-				res->clut.data=0;
-			res->clut.colors=num_palette;
+		case PNG_COLOR_TYPE_GRAY:
+		case PNG_COLOR_TYPE_PALETTE:
+//		case PNG_COLOR_TYPE_RGB:		// in theory, gImage can handle RGB pics - but enigma can't blit them, so
+										// until this is fixed, imho we should reject RGB pictures right here
+										// because later on we get an fatal blitting error followed by an enigma shutdown
+			res=new gImage(eSize(width, height), bit_depth);
 			
-			for (int i=0; i<num_palette; i++)
+			png_bytep *rowptr=new png_bytep[height];
+			for (unsigned int i=0; i<height; i++)
+				rowptr[i]=((png_byte*)(res->data))+i*res->stride;
+			png_read_rows(png_ptr, rowptr, 0, height);
+	
+			delete [] rowptr;
+			
+			if (color_type == PNG_COLOR_TYPE_PALETTE) 
 			{
-				res->clut.data[i].a=0;
-				res->clut.data[i].r=palette[i].red;
-				res->clut.data[i].g=palette[i].green;
-				res->clut.data[i].b=palette[i].blue;
-			}
-			if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
-			{
-				png_byte *trans;
-				png_get_tRNS(png_ptr, info_ptr, &trans, &num_palette, 0);
+				// indexed pictures without a palette make no sense
+				if (!png_get_valid(png_ptr, info_ptr, PNG_INFO_PLTE))
+					goto pngerror;
+
+				png_color *palette;
+				int num_palette;
+				png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette);
+				if (num_palette) 
+				{
+					res->clut.data=new gRGB[num_palette];
+					res->clut.colors=num_palette;
+				}
+			
 				for (int i=0; i<num_palette; i++)
-					res->clut.data[i].a=255-trans[i];
+				{
+					res->clut.data[i].a=0;
+					res->clut.data[i].r=palette[i].red;
+					res->clut.data[i].g=palette[i].green;
+					res->clut.data[i].b=palette[i].blue;
+				}
+				if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+				{
+					png_byte *trans;
+					png_color_16 *transparent_color;
+					
+					png_get_tRNS(png_ptr, info_ptr, &trans, &num_palette, &transparent_color);
+					if (transparent_color)
+						res->clut.data[transparent_color->index].a=255;
+					if (trans)					
+						for (int i=0; i<num_palette; i++)
+							res->clut.data[i].a=255-trans[i];
+				}
 			}
-		} else
-		{
-			res->clut.data=0;
-			res->clut.colors=0;
-		}
-		png_read_end(png_ptr, end_info);
-	} else
-		res=0;
+			break;
+		default:
+			eDebug("[ePNG] unsupported color_type in %s", filename);
+			goto pngerror2;
+	}
 
 	png_destroy_read_struct(&png_ptr, &info_ptr,&end_info);
 	fclose(fp);
 	return res;
+
+pngerror:
+	eDebug("[ePNG] png structure failure in %s\n", filename);
+pngerror2:
+	if (res)
+		delete res;
+	if (png_ptr)
+		png_destroy_read_struct(&png_ptr, info_ptr ? &info_ptr : (png_infopp)NULL, end_info ? &end_info : (png_infopp)NULL);
+	if (fp)
+		fclose(fp);
+	return 0;
 }
 
 int savePNG(const char *filename, gPixmap *pixmap)
