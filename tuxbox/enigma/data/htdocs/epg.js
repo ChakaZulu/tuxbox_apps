84822742ddee677c3156d1ff234200b117582edf
digi_casi <digi_casi>
2005-06-30 15:02:25 +0000

    webif: fix for timers generated from epg windows (thnx to nobody611)

--- tuxbox/enigma/data/htdocs/epg.js
+++ tuxbox/enigma/data/htdocs/epg.js
@@ -1,6 +1,6 @@
 function record(ref, start, duration, description, channel)
 {
-	NewWindow('/addTimerEvent?ref=' + ref + 'start=' + start + '&duration=' + duration + '&descr=' + description + '&channel=' + channel, 'record', '200', '200', 'no', '5000');
+	NewWindow('/addTimerEvent?ref=' + ref + '&start=' + start + '&duration=' + duration + '&descr=' + description + '&channel=' + channel, 'record', '200', '200', 'no', '5000');
 }
 
 function EPGDetails(xy)
