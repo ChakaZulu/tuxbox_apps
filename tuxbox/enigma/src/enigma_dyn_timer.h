4f67c7bf0fb64748b15a53f0942d6575099ac8ef
digi_casi <digi_casi>
2005-07-17 13:58:40 +0000

    webif: restructure code a bit

--- tuxbox/enigma/src/enigma_dyn_timer.h
+++ tuxbox/enigma/src/enigma_dyn_timer.h
@@ -1,8 +1,8 @@
-#ifndef __enigma_dyn_timer_h
-#define __enigma_dyn_timer_h
+#ifndef __enigma_dyn_pda_h
+#define __enigma_dyn_pda_h
 
 class eHTTPDynPathResolver;
-void ezapTimerInitializeDyn(eHTTPDynPathResolver *dyn_resolver, bool lockWeb);
+void ezapPDAInitializeDyn(eHTTPDynPathResolver *dyn_resolver, bool lockWeb);
 eString getTimerList(eString format);
 
 #endif /* __enigma_dyn_timer_h */
