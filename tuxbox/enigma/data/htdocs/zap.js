79cbd175bc788f82364fe044fec13b13a5b3eab4
digi_casi <digi_casi>
2005-07-25 16:10:55 +0000

    webif 4.5.0

--- tuxbox/enigma/data/htdocs/zap.js
+++ tuxbox/enigma/data/htdocs/zap.js
@@ -96,7 +96,6 @@ function loadBouquets(bouquet)
 	}
 	document.channelselector.bouquet.selectedIndex = bouquet;
 }
-
 function vlcStop()
 {
 	vlccmd.location = "http://127.0.0.1:8000/?control=stop";
@@ -125,7 +124,7 @@ function vlcStartItem()
 	vlccmd.location.href = "http://127.0.0.1:8080/?control=play&item=0";
 	setTimeout("setStreamingServiceRef()", 200);
 }
-unction setStreamingServiceRef()
+function setStreamingServiceRef()
 {
 	if (parent.data.serviceReference)
 		document.location = "/cgi-bin/setStreamingServiceRef?sref=" + parent.data.serviceReference;
