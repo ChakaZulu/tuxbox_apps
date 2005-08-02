a697b46e7801583dd2c357e7d859430ca1a28df2
ghostrider <ghostrider>
2005-08-02 22:36:56 +0000

    fix typo.. and show file sharing entry only on dm7000 or dm7020

--- tuxbox/enigma/src/setup_extra.cpp
+++ tuxbox/enigma/src/setup_extra.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * $Id: setup_extra.cpp,v 1.27 2005/08/02 21:10:47 ghostrider Exp $
+ * $Id: setup_extra.cpp,v 1.28 2005/08/02 22:36:56 ghostrider Exp $
  */
 #include <enigma.h>
 #include <setup_extra.h>
@@ -100,7 +100,9 @@ void eExpertSetup::init_eExpertSetup()
 	CONNECT((new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Use http authentification"), "/ezap/webif/lockWebIf", _("enables the http (user/password) authentification")))->selected, eExpertSetup::reinitializeHTTPServer );
 	CONNECT((new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Don't open serial port"), "/ezap/extra/disableSerialOutput", _("don't write debug messages to /dev/tts/0")))->selected, eExpertSetup::reinitializeHTTPServer );
 	new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Auto bouquet change"), "/elitedvb/extra/autobouquetchange", _("change into next bouquet when end of current bouquet is reached"));
-	new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Disable Samba"), "/elitedvb/network/samba", _("dont start samba daemons on tartup"));
+	if ( eSystemInfo::getInstance()->getHwType() == eSystemInfo::DM7000 ||
+	    eSystemInfo::getInstance()->getHwType() == eSystemInfo::DM7020)
+		new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Disable file sharing"), "/elitedvb/network/samba", _("don't start file sharing(samba) on startup"));
 #ifndef TUXTXT_CFG_STANDALONE
 	CONNECT((new eListBoxEntryCheck( (eListBox<eListBoxEntry>*)&list, _("Disable teletext caching"), "/ezap/extra/teletext_caching", _("don't cache teletext pages in background")))->selected, eExpertSetup::tuxtxtCachingChanged );
 #endif
