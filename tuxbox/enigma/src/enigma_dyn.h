907dc1a9743f902779e976c2a98805730f957051
digi_casi <digi_casi>
2005-08-06 20:56:36 +0000

    *** empty log message ***

--- tuxbox/enigma/src/enigma_dyn.h
+++ tuxbox/enigma/src/enigma_dyn.h
@@ -2,9 +2,9 @@
 #define __enigma_dyn_h
 
 #if ENABLE_DYN_MOUNT && ENABLE_DYN_CONF && ENABLE_DYN_FLASH && ENABLE_DYN_ROTOR
-#define WEBIFVERSION "4.6.1-Expert"
+#define WEBIFVERSION "4.6.2-Expert"
 #else
-#define WEBIFVERSION "4.6.1"
+#define WEBIFVERSION "4.6.2"
 #endif
 
 #define ZAPMODETV 0
