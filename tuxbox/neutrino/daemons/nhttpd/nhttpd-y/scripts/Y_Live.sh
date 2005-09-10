b2a50f6feb9820c85b8f64b0a561282095052b3f
yjogol <yjogol>
2005-09-10 12:51:31 +0000

    yWeb: new Live radio switching

--- tuxbox/neutrino/daemons/nhttpd/nhttpd-y/scripts/Y_Live.sh
+++ tuxbox/neutrino/daemons/nhttpd/nhttpd-y/scripts/Y_Live.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # -----------------------------------------------------------
 # Live (yjogol)
-# $Date: 2005/09/04 12:41:29 $
-# $Revision: 1.3 $
+# $Date: 2005/09/10 12:51:31 $
+# $Revision: 1.4 $
 # -----------------------------------------------------------
 
 . /share/tuxbox/neutrino/httpd-y/scripts/_Y_Globals.sh
@@ -42,56 +42,104 @@ live_unlock()
 # -----------------------------------------------------------
 prepare_tv()
 {
-		# switch to TV
-		wget -O - -q "$y_url_control/setmode?tv" >/dev/null
 		# SPTS on
 		wget -O - -q "$y_url_control/system?setAViAExtPlayBack=spts" >/dev/null
 }
+# -----------------------------------------------------------
+prepare_radio()
+{
+		# SPTS on
+		wget -O - -q "$y_url_control/system?setAViAExtPlayBack=pes" >/dev/null
+}
 
+# -----------------------------------------------------------
+live_getmode()
+{
+	mode=`wget -O - -q "$y_url_control/getmode"`
+	mode=`echo "$mode"|sed -e "s/tv.*/tv/1"`
+	echo "$mode"
+}
+# -----------------------------------------------------------
+live_buildpage()
+{
+	mode=`live_getmode`
+	if [ "$mode" = "tv" ]
+	then
+		prepare_tv
+		url=`buildStreamingURL`
+		case "$2" in
+			ie)
+				buildHTML=`sed -e s/Y_URL/$url/g $y_path_httpd/Y_Live_IE_tmpl.htm` ;;
+			moz)
+				buildHTML=`sed -e s/Y_URL/$url/g $y_path_httpd/Y_Live_Moz_tmpl.htm` ;;
+		esac
+		echo "$buildHTML"
+	else
+		prepare_radio
+		url=`buildStreamingAudioRawURL`
+#		echo "url:($url) m3u:($y_tmp_m3u)"
+		echo "$url" > $y_tmp_m3u
+		echo "$url" > $y_tmp_m4u
+		cat $y_path_httpd/Y_Live_Radio.htm
+	fi
+}
+
+live_buildpanel()
+{
+	# actual Bouquet
+	actualBouquet=`wget -O - -q $y_url_control/getbouquet?actual`
+	if [ "$2" = "" ]
+	then
+		bouquet="$actualBouquet"
+	else
+		bouquet="$2"
+	fi
+		bouquets=`buildHTMLbouquets $bouquet`
+	echo "$bouquets" >$y_tmp
+	buildHTML=`sed "/Y_Bouquets/r $y_tmp" $y_path_httpd/Y_Live_panel_tmpl.htm`
+		channels=`buildHTMLbouquetChannels $bouquet`
+	actualChannel=`wget -O - -q $y_url_control/zapto`
+	channels=`echo $channels|sed -e "s/$actualChannel/& selected/g"`
+	echo "$channels" >$y_tmp
+	buildHTML=`echo "$buildHTML"|sed "/Y_Channels/r $y_tmp"`
+
+	mode=`live_getmode`
+	if [ "$mode" = "tv" ]
+	then
+		ymode="Radio"
+	else
+		ymode="TV"
+	fi
+	buildHTML=`echo "$buildHTML"|sed -e "s/Y_mode/$ymode/g"`
+
+	echo "$buildHTML"
+	echo "" >$y_tmp
+}
 # -----------------------------------
 # Main
 # -----------------------------------
 case "$1" in
 	panel)
-		# actual Bouquet
-		actualBouquet=`wget -O - -q $y_url_control/getbouquet?actual`
-		if [ "$2" = "" ]
-		then
-			bouquet="$actualBouquet"
-		else
-			bouquet="$2"
-		fi
-		
-		bouquets=`buildHTMLbouquets $bouquet`
-		echo "$bouquets" >$y_tmp
-		buildHTML=`sed "/Y_Bouquets/r $y_tmp" $y_path_httpd/Y_Live_panel_tmpl.htm`
-
-		channels=`buildHTMLbouquetChannels $bouquet`
-		actualChannel=`wget -O - -q $y_url_control/zapto`
-		channels=`echo $channels|sed -e "s/$actualChannel/& selected/g"`
-		echo "$channels" >$y_tmp
-		buildHTML=`echo "$buildHTML"|sed "/Y_Channels/r $y_tmp"`
-
-		echo "$buildHTML"
-		echo "" >$y_tmp
-		;;
+		live_buildpanel	;;
 
 	zapto)
 		wget -O - -q "$y_url_control/zapto?$2" >/dev/null
 		cat $y_wait_live
 		;;
-	ie)
-		prepare_tv
-		url=`buildStreamingURL`
-		buildHTML=`sed -e s/Y_URL/$url/g $y_path_httpd/Y_Live_IE_tmpl.htm`
-		echo "$buildHTML"
+
+	switchto)
+		if [ "$2" = "Radio" ]
+		then
+			wget -O - -q "$y_url_control/setmode?radio" >/dev/null
+		else
+			wget -O - -q "$y_url_control/setmode?tv" >/dev/null
+		fi
+		cat $y_path_httpd/Y_LiveView.htm
 		;;
-	moz)
-		prepare_tv
-		url=`buildStreamingURL`
-		cmd="sed -e s/Y_URL/$url/g $y_path_httpd/Y_Live_Moz_tmpl.htm"
-		buildHTML=`$cmd`
-		echo $buildHTML ;;
+
+	live)
+		live_buildpage $*
+		;;
 
 	url)
 		url=`buildStreamingRawURL`
