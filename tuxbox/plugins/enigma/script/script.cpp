5c488d5f24332f1f19763396d81a7c04c9b22158
timekiller <timekiller>
2005-09-16 19:38:22 +0000

    update to new version

--- tuxbox/plugins/enigma/script/script.cpp
+++ tuxbox/plugins/enigma/script/script.cpp
@@ -20,15 +20,60 @@
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
 */
 
-#include <map>
 #include <plugin.h>
+#include <stdio.h>
+#include <signal.h>
+#include <lib/gui/ewindow.h>
+#include <lib/gui/ebutton.h>
+#include <lib/gui/emessage.h>
 
-#define SCRIPT "/var/etc/script.sh&"
-
-extern "C" int plugin_exec(PluginParam *par);
-
-int plugin_exec(PluginParam *par)
+class eScriptWindow: public eWindow
 {
-	return system(SCRIPT);
+	eButton *bt_scripts[10];
+	void runScript(int i);
+public:
+	eScriptWindow();
+	~eScriptWindow();
+};
+
+extern "C" int plugin_exec( PluginParam *par )
+{
+	eScriptWindow dlg;
+	dlg.show();
+	int result=dlg.exec();
+	dlg.hide();
+	return result;
 }
 
+eScriptWindow::eScriptWindow(): eWindow(1)
+{
+	cmove(ePoint(100, 100));
+	cresize(eSize(520, 376));
+	setText((_("Script Plugin")));
+	
+	for(int i=1; i<10; i++)
+	{
+		bt_scripts[i-1]=new eButton(this);
+		bt_scripts[i-1]->move(ePoint(10, 10+((i-1)*32)));
+		bt_scripts[i-1]->resize(eSize(clientrect.width()-20, 30));
+		bt_scripts[i-1]->setShortcut(eString().sprintf("%d",i));
+		bt_scripts[i-1]->setShortcutPixmap(eString().sprintf("%d",i));
+		bt_scripts[i-1]->loadDeco();
+		bt_scripts[i-1]->setText(eString().sprintf("Script %d (/var/bin/script%.02d.sh)", i, i));
+		CONNECT_1_0(bt_scripts[i-1]->selected, eScriptWindow::runScript, i);
+	}
+	
+	setFocus(bt_scripts[0]);
+}
+
+eScriptWindow::~eScriptWindow()
+{
+}
+
+void eScriptWindow::runScript(int i)
+{
+	char cmd[256];
+	sprintf(cmd, "/var/bin/script%.02d.sh&", i);
+	eDebug("Running %s\n", cmd);
+	system(cmd);	
+}
