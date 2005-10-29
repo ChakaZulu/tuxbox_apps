f3cbb044b2afb04a05094046556f95dfe4867e65
obi <obi>
2005-10-29 00:10:07 +0000

    changed license to LGPL 2.1

--- dvb/libdvbsi++/include/dvbsi++/network_name_descriptor.h
+++ dvb/libdvbsi++/include/dvbsi++/network_name_descriptor.h
@@ -1,22 +1,13 @@
 /*
- * $Id: network_name_descriptor.h,v 1.1 2004/02/13 15:27:38 obi Exp $
+ * $Id: network_name_descriptor.h,v 1.2 2005/10/29 00:10:08 obi Exp $
  *
- * Copyright (C) 2002-2004 Andreas Oberritter <obi@saftware.de>
+ * Copyright (C) 2002-2005 Andreas Oberritter <obi@saftware.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * See the file 'COPYING' in the top level directory for details.
  */
 
 #ifndef __network_name_descriptor_h__
--- dvb/libdvbsi++/src/descriptor_container.cpp
+++ dvb/libdvbsi++/src/descriptor_container.cpp
@@ -1,22 +1,13 @@
 /*
- * $Id: descriptor_container.cpp,v 1.4 2005/09/30 16:13:49 ghostrider Exp $
+ * $Id: descriptor_container.cpp,v 1.5 2005/10/29 00:10:16 obi Exp $
  *
- * Copyright (C) 2002-2004 Andreas Oberritter <obi@saftware.de>
+ * Copyright (C) 2002-2005 Andreas Oberritter <obi@saftware.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * See the file 'COPYING' in the top level directory for details.
  */
 
 #include <dvbsi++/ac3_descriptor.h>
