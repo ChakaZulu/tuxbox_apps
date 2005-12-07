567ac3ce1b1a40c5b7ca7ceaff565f0cdafd848f
mws <mws>
2005-12-07 19:06:40 +0000

    after discussion with ghostrider we chose to perform all vector filling via memcpy and if a vector ctor(size) is not applicable within the initializer list, we use vector.resize(size)

--- dvb/libdvbsi++/src/linkage_descriptor.cpp
+++ dvb/libdvbsi++/src/linkage_descriptor.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: linkage_descriptor.cpp,v 1.7 2005/12/06 23:51:25 ghostrider Exp $
+ * $Id: linkage_descriptor.cpp,v 1.8 2005/12/07 19:06:40 mws Exp $
  *
  * Copyright (C) 2002-2005 Andreas Oberritter <obi@saftware.de>
  *
@@ -22,8 +22,8 @@ LinkageDescriptor::LinkageDescriptor(const uint8_t * const buffer) : Descriptor(
 
 	if (linkageType != 0x08)
 	{
-		privateDataBytes.reserve(descriptorLength - 7);
-		privateDataBytes.insert(privateDataBytes.begin(), buffer+9, buffer+9+descriptorLength-7);
+		privateDataBytes.resize(descriptorLength - 7);
+		memcpy(&privateDataBytes[0], buffer+9, descriptorLength-7);
 	}
 	else {
 		uint8_t offset = 0;
@@ -41,8 +41,8 @@ LinkageDescriptor::LinkageDescriptor(const uint8_t * const buffer) : Descriptor(
 			offset += 2;
 		}
 		bytes = descriptorLength-offset-8;
-		privateDataBytes.reserve(bytes);
-		privateDataBytes.insert(privateDataBytes.begin(), buffer+offset+10, buffer+offset+10+bytes);
+		privateDataBytes.resize(bytes);
+		memcpy(&privateDataBytes[0], buffer+offset+10, bytes);
 	}
 }
 
