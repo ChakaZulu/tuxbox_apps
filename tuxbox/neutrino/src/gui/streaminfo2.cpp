c1f69b9cc800bb92c05ecfbfeeba3c3c35f08074
metallica <metallica>
2005-12-16 12:55:52 +0000

    fix delete ;)

--- tuxbox/neutrino/src/gui/streaminfo2.cpp
+++ tuxbox/neutrino/src/gui/streaminfo2.cpp
@@ -86,7 +86,7 @@ CStreamInfo2::CStreamInfo2()
 
 CStreamInfo2::~CStreamInfo2()
 {
-	delete pig;
+	//destructor funktioniert hier nicht richtig 
 }
 
 int CStreamInfo2::exec(CMenuTarget* parent, const std::string &)
@@ -102,7 +102,7 @@ int CStreamInfo2::exec(CMenuTarget* parent, const std::string &)
 	doSignalStrengthLoop ();
 
 	hide();
-
+	delete pig;
 	res = menu_return::RETURN_EXIT_ALL;
 	return res;
 }
