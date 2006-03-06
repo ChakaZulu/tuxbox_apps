c9a552aaf97582582249249733783b71132b9449
rasc <rasc>
2006-03-06 20:32:35 +0000

    DSM-CC Carousel, lots of Bugfixes, BIOP::Message not yet decodable (ddb has to collect Modules)

--- dvb/dvbsnoop/src/datacarousel/dsmcc_ddb.c
+++ dvb/dvbsnoop/src/datacarousel/dsmcc_ddb.c
@@ -1,5 +1,5 @@
 /*
-$Id: dsmcc_ddb.c,v 1.3 2006/01/02 18:23:47 rasc Exp $
+$Id: dsmcc_ddb.c,v 1.4 2006/03/06 20:32:35 rasc Exp $
 
 
  DVBSNOOP
@@ -14,6 +14,9 @@ $Id: dsmcc_ddb.c,v 1.3 2006/01/02 18:23:47 rasc Exp $
 
 
 $Log: dsmcc_ddb.c,v $
+Revision 1.4  2006/03/06 20:32:35  rasc
+DSM-CC Carousel, lots of Bugfixes, BIOP::Message not yet decodable (ddb has to collect Modules)
+
 Revision 1.3  2006/01/02 18:23:47  rasc
 just update copyright and prepare for a new public tar ball
 
@@ -77,7 +80,11 @@ int dsmcc_DownloadDataMessage (int v, u_char *b, u_int len)
 	len -= 6;
 
 	print_databytes (v, "Block Data: ", b, len);
-	// $$$ Save Blockdata if option set to file
+
+	// $$$ TODO: Save Blockdata if option set to file
+	// a good Test transponder is ASTRA's MHP carousel
+	// Beware of multiple BIOPs in a Module! (read below!)
+
 
 	return len_org;
 }
