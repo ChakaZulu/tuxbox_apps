8819ffa83daa4d97e754d138ca72765d4811fc86
mws <mws>
2006-09-26 20:52:09 +0000

    add running status section - even if i never received one :) adjusted Makefile.am

---
+++ dvb/libdvbsi++/include/dvbsi++/running_status_section.h
@@ -0,0 +1,60 @@
+/*
+ *  $Id: running_status_section.h,v 1.1 2006/09/26 20:52:09 mws Exp $
+ *
+ *  Copyright (C) 2006 Marcel Siegert <mws@twisted-brains.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * See the file 'COPYING' in the top level directory for details.
+ */
+#ifndef __running_status_section_h__
+#define __running_status_section_h__
+
+#include "short_section.h"
+
+class RunningStatus
+{
+	protected:
+		unsigned transportStreamId		: 16;
+		unsigned originalNetworkId		: 16;
+		unsigned serviceId			: 16;
+		unsigned eventId			: 16;
+		unsigned runningStatus			:  3;
+	public:
+		RunningStatus(const uint8_t* const buffer);
+		virtual ~RunningStatus(){}
+
+		uint16_t getTransportStreamId(void) const;
+		uint16_t getOriginalNetworkId(void) const;
+		uint16_t getServiceId(void) const;
+		uint16_t getEventId(void) const;
+		uint8_t getRunningStatus(void) const;
+};
+
+typedef std::list<RunningStatus*> RunningStatusList;
+typedef RunningStatusList::iterator RunningStatusIterator;
+typedef RunningStatusList::const_iterator RunningStatusConstIterator;
+
+class RunningStatusSection : public ShortSection
+{
+	protected:
+		RunningStatusList runningStatus;
+
+	public:
+		RunningStatusSection(const uint8_t* const buffer);
+		virtual ~RunningStatusSection();
+
+		static const enum PacketId PID = PID_RST;
+		static const enum TableId TID = TID_RST;
+		static const uint32_t TIMEOUT = 36000;
+
+		const RunningStatusList& getRunningStatus(void) const;
+};
+
+typedef std::list<RunningStatusSection*> RunningStatusSectionList;
+typedef RunningStatusSectionList::iterator RunningStatusSectionIterator;
+typedef RunningStatusSectionList::const_iterator RunningStatusSectionConstIterator;
+
+#endif /* __running_status_section_h__ */
---
+++ dvb/libdvbsi++/src/running_status_section.cpp
@@ -0,0 +1,68 @@
+/*
+ * $Id: running_status_section.cpp,v 1.1 2006/09/26 20:52:09 mws Exp $
+ *
+ * Copyright (C) 2006 Marcel Siegert <mws@twisted-brains.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * See the file 'COPYING' in the top level directory for details.
+ */
+#include <dvbsi++/byte_stream.h>
+#include <dvbsi++/running_status_section.h>
+
+RunningStatus::RunningStatus(const uint8_t* const buffer)
+{
+	transportStreamId = UINT16(&buffer[0]);
+	originalNetworkId = UINT16(&buffer[2]);
+	serviceId = UINT16(&buffer[4]);
+	eventId = UINT16(&buffer[6]);
+	runningStatus = UINT16(&buffer[8]) & 0x03;
+}
+
+uint16_t RunningStatus::getTransportStreamId(void) const
+{
+	return transportStreamId;
+}
+
+uint16_t RunningStatus::getOriginalNetworkId(void) const
+{
+	return originalNetworkId;
+}
+
+uint16_t RunningStatus::getServiceId(void) const
+{
+	return serviceId;
+}
+
+uint16_t RunningStatus::getEventId(void) const
+{
+	return eventId;
+}
+
+uint8_t RunningStatus::getRunningStatus(void) const
+{
+	return runningStatus;
+}
+
+RunningStatusSection::RunningStatusSection(const uint8_t* const buffer) : ShortSection(buffer)
+{
+	for (size_t i = 0; i < sectionLength; i += 9)
+	{
+		runningStatus.push_back(new RunningStatus(&buffer[3 + i]));
+	}
+}
+
+RunningStatusSection::~RunningStatusSection(void)
+{
+	for( RunningStatusList::iterator it = runningStatus.begin(); it != runningStatus.end(); ++it)
+	{
+		delete *it;
+	}
+}
+
+const RunningStatusList& RunningStatusSection::getRunningStatus() const
+{
+	return runningStatus;
+}
