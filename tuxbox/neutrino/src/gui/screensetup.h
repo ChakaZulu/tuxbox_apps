4caec7f7559eef48200dde1f7f4cb83e68d2477e
dbt <dbt>
2007-02-25 17:16:48 +0000

    see sreensetup.cpp

--- tuxbox/neutrino/src/gui/screensetup.h
+++ tuxbox/neutrino/src/gui/screensetup.h
@@ -53,6 +53,7 @@ class CScreenSetup : public CMenuTarget
     void paintBorderLR();
     void paintCoords();
     void paintBorder(int selected);
+ 	void paintIcons();
 	
  public:
     CScreenSetup();
