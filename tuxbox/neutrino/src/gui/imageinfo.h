f2abfc4a3e5067c35a23a69230c16df64333fbe7
dbt <dbt>
2007-03-31 09:00:20 +0000

    -überarbeitete Imageimformationen -optisch überarbeitet -Abruf bestimmter Infos mittels Buttons -neben Standardausgabe der ./version -Inhalte können auch  auch Einträge wie 	subversion= 	cvs= 	info=  angezeigt werden -Ausgabe der Chip-Informationen (1x, 2x) -Ausgabe einiger Versionsdetails des Images (Kernel, BusyBox...) -Ausgabe der Partitionen

--- tuxbox/neutrino/src/gui/imageinfo.h
+++ tuxbox/neutrino/src/gui/imageinfo.h
@@ -1,6 +1,7 @@
 /*
 	Neutrino-GUI  -   DBoxII-Project
-
+	
+	$Id: imageinfo.h,v 1.2 2007/03/31 09:00:20 dbt Exp $
 
 	License: GPL
 
@@ -23,6 +24,8 @@
 #ifndef __imageinfo__
 #define __imageinfo__
 
+#include <string>
+
 #include <configfile.h>
 
 #include <gui/widget/menue.h>
@@ -30,30 +33,65 @@
 #include <driver/framebuffer.h>
 #include <driver/pig.h>
 
+//Version InfoTypes
+#define VERSION_IMAGENAME 1
+#define VERSION_CREATOR 2
+#define VERSION_HOMEPAGE 3
+#define VERSION_REVISION 4
+#define VERSION_SUBVERSION	5
+#define VERSION_CVSLEVEL 6
+#define VERSION_DISTRIBUTION 7
+#define VERSION_RELCYCLE 8
+#define VERSION_TYPE 9
+#define VERSION_DATE 10
+#define VERSION_INFO 11
+
 class CImageInfo : public CMenuTarget
 {
 	private:
 		CConfigFile     * configfile;
 		CFrameBuffer	*frameBuffer;
-		int x;
-		int y;
-		int ypos;
-		int width;
-		int height;
-		int hheight,iheight,sheight; 	// head/info/small font height
 
+		int x, y;
+		int xpos, ypos; //line starts
+		int width, height;
+		int hheight,iheight,sheight; // head/info/small font height
+
+		int startX, startY; //boxposition
+		int endX, endY;
+	
 		int max_height;	// Frambuffer 0.. max
 		int max_width;
+		
+		int pigw; //picbox dimensions
+	    int pigh;
 
 		int font_head;
 		int font_info;
 		int font_small;
-
+	
+		int x_offset_large; //offsets(space) between caption and infostrings
+		int x_offset_small;
+						
+		std::string nversion;
+		std::string sysstr;
+		std::string revstr;
+		std::string chiptype;
+		
+		const char *i_info;
+	
 		CPIG *pig;
-
+		
 		void paint();
 		void paint_pig(int x, int y, int w, int h);
 		void paintLine(int xpos, int font, const char* text);
+		void paintContent(int fontSize, int xpos, int ypos, const char* text);
+		void paintSupport(int y_startposition);
+		void paintLicense(int y_startposition);
+		void paintRevisionInfos(int y_startposition);
+		void paintPartitions(int y_startposition);
+		void clearContentBox();
+		std::string getRawInfos();
 
 	public:
 
@@ -61,8 +99,12 @@ class CImageInfo : public CMenuTarget
 		~CImageInfo();
 
 		void hide();
-		int exec(CMenuTarget* parent, const std::string & actionKey);
+		int exec(CMenuTarget* parent, const std::string& actionKey);
+		const char *ImageInfo( int InfoType );
+		std::string getChipInfo();
+		std::string getImageInfoVersion();
+		std::string getSysInfo(std::string infotag, bool reverse);
+				
 };
 
 #endif
-
