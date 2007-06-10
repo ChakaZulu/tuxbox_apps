fd1c95be2b53015c962e8d2fcb00fa99f1044fa6
houdini <houdini>
2007-06-10 20:12:09 +0000

    preparation of upnp commit, thanx @jochen_f + tabs/spaces cleanup

--- tuxbox/neutrino/src/gui/widget/icons.h
+++ tuxbox/neutrino/src/gui/widget/icons.h
@@ -2,7 +2,7 @@
 #define __gui_widget_icons_h__
 
 /*
- * $Header: /cvs/tuxbox/apps/tuxbox/neutrino/src/gui/widget/icons.h,v 1.12 2007/04/20 10:13:29 saruman Exp $
+ * $Header: /cvs/tuxbox/apps/tuxbox/neutrino/src/gui/widget/icons.h,v 1.13 2007/06/10 20:12:09 houdini Exp $
  *
  * (C) 2003 by thegoodguy <thegoodguy@berlios.de>
  *
@@ -24,57 +24,58 @@
 
 /* icon files */
 
-#define NEUTRINO_ICON_BUTTON_BLUE   "blau.raw"
-#define NEUTRINO_ICON_BUTTON_GREEN  "gruen.raw"
-#define NEUTRINO_ICON_BUTTON_RED    "rot.raw"
-#define NEUTRINO_ICON_BUTTON_YELLOW "gelb.raw"
+#define NEUTRINO_ICON_BUTTON_BLUE	"blau.raw"
+#define NEUTRINO_ICON_BUTTON_GREEN	"gruen.raw"
+#define NEUTRINO_ICON_BUTTON_RED	"rot.raw"
+#define NEUTRINO_ICON_BUTTON_YELLOW	"gelb.raw"
 
-#define NEUTRINO_ICON_BUTTON_DBOX         "dbox.raw"
-#define NEUTRINO_ICON_BUTTON_DBOX_SMALL   "dbox_small.raw"
-#define NEUTRINO_ICON_BUTTON_HELP         "help.raw"
-#define NEUTRINO_ICON_BUTTON_HELP_SMALL   "help_small.raw"
-#define NEUTRINO_ICON_BUTTON_HOME         "home.raw"
-#define NEUTRINO_ICON_BUTTON_MUTE         "mute.raw"
-#define NEUTRINO_ICON_BUTTON_MUTE_SMALL   "mute_small.raw"
+#define NEUTRINO_ICON_BUTTON_DBOX	"dbox.raw"
+#define NEUTRINO_ICON_BUTTON_DBOX_SMALL	"dbox_small.raw"
+#define NEUTRINO_ICON_BUTTON_HELP	"help.raw"
+#define NEUTRINO_ICON_BUTTON_HELP_SMALL	"help_small.raw"
+#define NEUTRINO_ICON_BUTTON_HOME	"home.raw"
+#define NEUTRINO_ICON_BUTTON_MUTE	"mute.raw"
+#define NEUTRINO_ICON_BUTTON_MUTE_SMALL	"mute_small.raw"
 
-#define NEUTRINO_ICON_BUTTON_OKAY         "ok.raw"
+#define NEUTRINO_ICON_BUTTON_OKAY	"ok.raw"
 
-#define NEUTRINO_ICON_BUTTON_0         "0.raw"
-#define NEUTRINO_ICON_BUTTON_1         "1.raw"
-#define NEUTRINO_ICON_BUTTON_2         "2.raw"
-#define NEUTRINO_ICON_BUTTON_3         "3.raw"
-#define NEUTRINO_ICON_BUTTON_4         "4.raw"
-#define NEUTRINO_ICON_BUTTON_5         "5.raw"
-#define NEUTRINO_ICON_BUTTON_6         "6.raw"
-#define NEUTRINO_ICON_BUTTON_7         "7.raw"
-#define NEUTRINO_ICON_BUTTON_8         "8.raw"
-#define NEUTRINO_ICON_BUTTON_9         "9.raw"
+#define NEUTRINO_ICON_BUTTON_0		"0.raw"
+#define NEUTRINO_ICON_BUTTON_1		"1.raw"
+#define NEUTRINO_ICON_BUTTON_2		"2.raw"
+#define NEUTRINO_ICON_BUTTON_3		"3.raw"
+#define NEUTRINO_ICON_BUTTON_4		"4.raw"
+#define NEUTRINO_ICON_BUTTON_5		"5.raw"
+#define NEUTRINO_ICON_BUTTON_6		"6.raw"
+#define NEUTRINO_ICON_BUTTON_7		"7.raw"
+#define NEUTRINO_ICON_BUTTON_8		"8.raw"
+#define NEUTRINO_ICON_BUTTON_9		"9.raw"
 
-#define NEUTRINO_ICON_BUTTON_TOP       "up.raw"
-#define NEUTRINO_ICON_BUTTON_RIGHT     "right.raw"
-#define NEUTRINO_ICON_BUTTON_DOWN      "down.raw"
-#define NEUTRINO_ICON_BUTTON_LEFT      "left.raw"
+#define NEUTRINO_ICON_BUTTON_TOP	"up.raw"
+#define NEUTRINO_ICON_BUTTON_RIGHT	"right.raw"
+#define NEUTRINO_ICON_BUTTON_DOWN	"down.raw"
+#define NEUTRINO_ICON_BUTTON_LEFT	"left.raw"
 
-#define NEUTRINO_ICON_ERROR         "error.raw"
-#define NEUTRINO_ICON_FILE          "file.raw"
-#define NEUTRINO_ICON_GAMES         "games.raw"
-#define NEUTRINO_ICON_INFO          "info.raw"
-#define NEUTRINO_ICON_MOUNTED       "mounted.raw"
-#define NEUTRINO_ICON_NOT_MOUNTED   "not_mounted.raw"
-#define NEUTRINO_ICON_RECORDING     "recording.raw"
-#define NEUTRINO_ICON_SETTINGS      "settings.raw"
-#define NEUTRINO_ICON_SHELL         "shell.raw"
+#define NEUTRINO_ICON_ERROR		"error.raw"
+#define NEUTRINO_ICON_FILE		"file.raw"
+#define NEUTRINO_ICON_GAMES		"games.raw"
+#define NEUTRINO_ICON_INFO		"info.raw"
+#define NEUTRINO_ICON_MOUNTED		"mounted.raw"
+#define NEUTRINO_ICON_NOT_MOUNTED	"not_mounted.raw"
+#define NEUTRINO_ICON_RECORDING		"recording.raw"
+#define NEUTRINO_ICON_SETTINGS		"settings.raw"
+#define NEUTRINO_ICON_SHELL		"shell.raw"
+#define NEUTRINO_ICON_UPNP		"upnp.raw"
 
 
 /* icon widths */
 
-#define NEUTRINO_ICON_BUTTON_BLUE_WIDTH   16
-#define NEUTRINO_ICON_BUTTON_GREEN_WIDTH  16
-#define NEUTRINO_ICON_BUTTON_RED_WIDTH    16
-#define NEUTRINO_ICON_BUTTON_YELLOW_WIDTH 16
+#define NEUTRINO_ICON_BUTTON_BLUE_WIDTH		16
+#define NEUTRINO_ICON_BUTTON_GREEN_WIDTH	16
+#define NEUTRINO_ICON_BUTTON_RED_WIDTH		16
+#define NEUTRINO_ICON_BUTTON_YELLOW_WIDTH	16
 
 /* misc */
 
-#define NEUTRINO_ICON_VARPATH         "var/share/tuxbox/neutrino/icons/"  //alternatively path for user-defined icons 
+#define NEUTRINO_ICON_VARPATH		"var/share/tuxbox/neutrino/icons/"  //alternatively path for user-defined icons 
 
 #endif /* __gui_widget_icons_h__ */
