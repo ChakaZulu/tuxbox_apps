c20f7db5a44a7980d5716d8f3b45ec23309a9c65
carjay <carjay>
2007-10-14 23:55:36 +0000

    - fb.h pulls in kernel types which clash with later (2.6.18) kernels unless   the glibc defines have already been included. Tested this with 2.4.34 so   it should no cause problems for older kernels.

--- tuxbox/enigma/lib/picviewer/fb_display.cpp
+++ tuxbox/enigma/lib/picviewer/fb_display.cpp
@@ -1,9 +1,9 @@
 #ifndef DISABLE_FILE
-#include <linux/fb.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <linux/fb.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
