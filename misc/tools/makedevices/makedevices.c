9e7d1b868a47ee2f85131b68abead726cc842d73
seife <seife>
2007-11-17 20:58:22 +0000

    Add "makedevices", to create the devices in the init script for kernels without devfs (2.6...)

---
+++ misc/tools/makedevices/makedevices.c
@@ -0,0 +1,106 @@
+/*  $Id
+    A small hack to create all needed device nodes in rcS.
+    Ten times faster than doing it in a script.
+
+    Copyright (C) 2007 Stefan Seyfried
+    All rights reserved.
+
+    This program is released under the GNU General Public License
+    (GPL) Version 2, not any other version of that license.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define C S_IFCHR
+#define B S_IFBLK
+
+struct deventry {
+	const char *name;
+	const int type;
+	const char major;
+	const char minor;
+};
+
+struct deventry devices[] = {
+	{ "mem",	C, 1, 1 },
+	{ "kmem",	C, 1, 2 },
+	{ "null",	C, 1, 3 },
+	{ "random",	C, 1, 8 },
+	{ "urandom",	C, 1, 9 },
+	{ "tty0",	C, 4, 0 },
+	{ "tty1",	C, 4, 2 },
+	{ "tty2",	C, 4, 2 },
+	{ "tty3",	C, 4, 3 },
+	{ "tts/0",	C, 4, 64 },
+	{ "tts/1",	C, 4, 65 },
+	{ "tty",	C, 5, 0 },
+	{ "console",	C, 5, 1 },
+	{ "ptmx",	C, 5, 2 },
+	{ "loop0",	B, 7, 0 },
+	{ "loop1",	B, 7, 1 },
+	{ "loop2",	B, 7, 2 },
+	{ "loop3",	B, 7, 3 },
+	{ "loop4",	B, 7, 4 },
+	{ "loop5",	B, 7, 5 },
+	{ "loop6",	B, 7, 6 },
+	{ "loop7",	B, 7, 7 },
+	{ "input/mouse0",	C, 13, 32 },
+	{ "input/mice",		C, 13, 63 },
+	{ "input/event0",	C, 13, 64 },
+	{ "sound/mixer",	C, 14, 0 },
+	{ "sound/dsp",		C, 14, 3 },
+	{ "sound/mixer1",	C, 14, 16 },
+	{ "fb0",	C, 29, 0 },
+	{ "mtdblock0",	B, 31, 0 },
+	{ "mtdblock1",	B, 31, 1 },
+	{ "mtdblock2",	B, 31, 2 },
+	{ "mtdblock3",	B, 31, 3 },
+	{ "mtdblock4",	B, 31, 4 },
+	{ "mtdblock5",	B, 31, 5 },
+	{ "v4l/video0",	C, 81, 0 },
+	{ "i2c/0",	C, 89, 0 },
+	{ "mtd/0",	B, 90, 0 },
+	{ "mtd/1",	B, 90, 2 },
+	{ "mtd/2",	B, 90, 4 },
+	{ "mtd/3",	B, 90, 6 },
+	{ "mtd/4",	B, 90, 8 },
+	{ "mtd/5",	B, 90, 10 },
+	{ "mtd/0ro",	B, 90, 1 },
+	{ "mtd/1ro",	B, 90, 3 },
+	{ "mtd/2ro",	B, 90, 5 },
+	{ "mtd/3ro",	B, 90, 7 },
+	{ "mtd/4ro",	B, 90, 9 },
+	{ "mtd/5ro",	B, 90, 11 },
+	{ "dvb/adapter0/video0",	C, 212, 0 },
+	{ "dvb/adapter0/audio0",	C, 212, 1 },
+	{ "dvb/adapter0/frontend0",	C, 212, 3 },
+	{ "dvb/adapter0/demux0",	C, 212, 4 },
+	{ "dvb/adapter0/dvr0",		C, 212, 5 },
+	{ "dvb/adapter0/ca0",		C, 212, 6 },
+	{ "dvb/adapter0/net0",		C, 212, 7 },
+	{ "dvb/adapter0/ca1",		C, 212, 22 },
+	{ NULL, 0, 0, 0 }
+};
+
+int main(void)
+{
+	int i = 0;
+	char devname[100];
+	while (devices[i].name) {
+		strcpy(devname, "/dev/");
+		strcat(devname, devices[i].name);
+		if (mknod(devname, devices[i].type|0600,
+			  makedev(devices[i].major,devices[i].minor)))
+			fprintf(stderr, "mknod %s: %s\n", devname,
+							strerror(errno));
+		i++;
+	}
+	return 0;
+}
