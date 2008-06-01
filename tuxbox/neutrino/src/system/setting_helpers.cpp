92a2126cc2eca89fb01ca3084a758397e4664779
ecosys <ecosys>
2008-06-01 12:18:54 +0000

    Hopefully fix this encoding sh** last checkin. (file ist saved in UTF-8)

--- tuxbox/neutrino/src/system/setting_helpers.cpp
+++ tuxbox/neutrino/src/system/setting_helpers.cpp
@@ -1,5 +1,5 @@
 /*
-	$Id: setting_helpers.cpp,v 1.167 2008/06/01 12:08:04 ecosys Exp $
+	$Id: setting_helpers.cpp,v 1.168 2008/06/01 12:18:54 ecosys Exp $
 
 	Neutrino-GUI  -   DBoxII-Project
 
@@ -816,7 +816,7 @@ int CUserMenuMenu::exec(CMenuTarget* parent, const std::string & actionKey)
 	menu.addItem(GenericMenuBack);
 	menu.addItem(GenericMenuSeparatorLine);
 	
-	CStringInputSMS name(LOCALE_USERMENU_NAME,    &g_settings.usermenu_text[button], 11, NONEXISTANT_LOCALE, NONEXISTANT_LOCALE, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz����/- ");
+	CStringInputSMS name(LOCALE_USERMENU_NAME,    &g_settings.usermenu_text[button], 11, NONEXISTANT_LOCALE, NONEXISTANT_LOCALE, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzäöüß/- ");
 	menu.addItem(new CMenuForwarder(LOCALE_USERMENU_NAME,    true, g_settings.usermenu_text[button],&name));
 	menu.addItem(GenericMenuSeparatorLine);
 	
