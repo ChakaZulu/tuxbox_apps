4bb558ade4db18f1db12b2f79b5423a3be1d4853
rhabarber1848 <rhabarber1848>
2009-08-17 09:22:48 +0000

    New LIRC commands are send when Neutrino starts and stops: http://tuxbox-forum.dreambox-fan.de/forum/viewtopic.php?p=369472#p369472

--- tuxbox/neutrino/src/neutrino.cpp
+++ tuxbox/neutrino/src/neutrino.cpp
@@ -1,5 +1,5 @@
 /*
-	$Id: neutrino.cpp,v 1.967 2009/08/11 09:59:59 rhabarber1848 Exp $
+	$Id: neutrino.cpp,v 1.968 2009/08/17 09:22:48 rhabarber1848 Exp $
 	
 	Neutrino-GUI  -   DBoxII-Project
 
@@ -2335,6 +2335,9 @@ void CNeutrinoApp::RealRun(CMenuWidget &mainMenu)
 
 	dprintf(DEBUG_NORMAL, "initialized everything\n");
 
+	CIRSend irs("neutrinoon");
+	irs.Send();
+
 	if (g_settings.vcr_AutoSwitch)
 	{
 		int val = 0;
@@ -3292,6 +3295,9 @@ void CNeutrinoApp::ExitRun(const bool write_si)
 				if (g_RCInput != NULL)
 					delete g_RCInput;
 
+				CIRSend irs("neutrinooff");
+				irs.Send();
+
 				exit(0);
 
 			}
@@ -3300,6 +3306,9 @@ void CNeutrinoApp::ExitRun(const bool write_si)
 			g_Timerd->shutdown();
 			if (g_RCInput != NULL)
 				delete g_RCInput;
+
+			CIRSend irs("neutrinooff");
+			irs.Send();
 
 			exit(0);
 
